66eafa87231d966a877635df6e3043e6e943fdbe
TrafficConeGod <36275551+TrafficConeGod@users.noreply.github.com>
2020-02-15 18:59:41 -0500

    add semi working coins

--- src/objects/Coin.gd
+++ src/objects/Coin.gd
@@ -1,4 +1,19 @@
 extends Sprite
 
+signal on_collect
+onready var body := KinematicBody2D.new()
+onready var shape := CollisionShape2D.new()
+
+func collect():
+	visible = false
+	emit_signal("on_collect")
+
 func _ready():
 	texture = load("res://assets/textures/items/coins/yellow.png")
+	shape.shape = RectangleShape2D.new()
+	body.add_child(shape)
+	add_child(body)
+
+func _physics_process(delta):
+	if body.test_move(transform, Vector2(0, 0)):
+		collect()
