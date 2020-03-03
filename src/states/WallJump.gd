1094c107a628b355299179503f2ab60311b8a813
SuperMakerPlayer <51249300+SuperMakerPlayer@users.noreply.github.com>
2020-03-03 16:43:36 +0100

    stop character from being able to climb up walls

--- src/states/WallJump.gd
+++ src/states/WallJump.gd
@@ -7,9 +7,12 @@ export var walljump_power = Vector2(350, 320)
 var press_buffer = 0.0
 var wall_jump_timer = 0.0
 var direction_on_wj = 1
+var position_on_wj = Vector2(0, 0)
+var limit_y = false
+var character_in_range = false
 
 func _start_check(delta):
-	return character.state == character.get_state_instance("WallSlide") && press_buffer > 0 && character.state != character.get_state_instance("Bonked")
+	return character.state == character.get_state_instance("WallSlide") && !(limit_y && character.position.y < position_on_wj.y && character_in_range) && press_buffer > 0 && character.state != character.get_state_instance("Bonked")
 
 func _start(delta):
 	var jump_player = character.get_node("JumpSoundPlayer")
@@ -23,6 +26,8 @@ func _start(delta):
 	wall_jump_timer = 0.45
 	jump_player.play()
 	character.jump_animation = 0
+	limit_y = true
+	position_on_wj = character.position
 	pass
 
 func _update(delta):
@@ -43,6 +48,14 @@ func _stop_check(delta):
 	return wall_jump_timer <= 0 or character.is_walled() or character.is_grounded()
 	
 func _general_update(delta):
+	if character.position.x > position_on_wj.x - 3 && character.position.x < position_on_wj.x + 3:
+		character_in_range = true
+		if press_buffer > 0:
+			press_buffer = 0.075
+	else:
+		character_in_range = false
+	if character.is_grounded():
+		limit_y = false
 	if Input.is_action_just_pressed("jump") && !character.is_grounded():
 		press_buffer = 0.075
 	if press_buffer > 0:
