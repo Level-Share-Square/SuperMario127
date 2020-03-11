08e0781709ab20670f019093b85103c4d2e47b54
TrafficConeGod <36275551+TrafficConeGod@users.noreply.github.com>
2020-03-11 15:45:47 -0400

    add loading

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,1 +1,6 @@
-extends Node2D
+extends LevelDataLoader
+
+func _ready():
+	var data = LevelData.new()
+	data.load_in(load("res://assets/level_data/test_level.tres").contents)
+	load_in(data, data.areas[0])
---
+++ scenes/shared/background.gd
@@ -0,0 +1,4 @@
+extends CanvasLayer
+
+func load_in(level_data : LevelData, level_area : LevelArea):
+	print(level_area.settings.background)
