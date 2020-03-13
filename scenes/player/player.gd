60c3f1243d91018ff23cb9b3d9a92253b182e248
TrafficConeGod <36275551+TrafficConeGod@users.noreply.github.com>
2020-03-13 13:18:20 -0400

    add reloading

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -4,3 +4,7 @@ func _ready():
 	var data = LevelData.new()
 	data.load_in(load("res://assets/level_data/test_level.tres").contents)
 	load_in(data, data.areas[0])
+	
+func _process(delta):
+	if Input.is_action_just_pressed("reload"):
+		get_tree().reload_current_scene()
