c439f7765ccd7e841b7734dee5a3f2dec8c61b2f
TrafficConeGod <36275551+TrafficConeGod@users.noreply.github.com>
2020-03-13 15:20:11 -0400

    add ui

---
+++ scenes/editor/PlaceableItem.gd
@@ -0,0 +1,8 @@
+extends Node
+
+class_name PlaceableItem
+
+export var tileset_id := 0
+export var tile_id := 0
+export var object_id := 0
+export var is_object := false
--- scenes/editor/editor.gd
+++ scenes/editor/editor.gd
@@ -1,5 +1,9 @@
 extends LevelDataLoader
 
+export var placeable_items : NodePath
+
+onready var placeable_items_node = get_node(placeable_items)
+
 func _ready():
 	var data = LevelData.new()
 	data.load_in(load("res://assets/level_data/test_level.tres").contents)
