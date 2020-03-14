23a9c18b1ed4655a192ce6640833ca243d8b9892
SuperMakerPlayer <51249300+SuperMakerPlayer@users.noreply.github.com>
2020-03-14 12:52:29 +0100

    change play button

--- scenes/other/mode_switcher_button.gd
+++ scenes/other/mode_switcher_button.gd
@@ -1,18 +1,23 @@
 extends TextureButton
 
-onready var top_part = $TopPart
 onready var tween = $Tween
-onready var tween_bottom = $TweenBottom
+onready var tween_hover = $TweenHover
+onready var tween_coin = $TweenCoin
+onready var tween_disappear = $TweenDisappear
+onready var coin = $Coin
 onready var sound = $Sound
+onready var hover_sound = $HoverSound
 onready var fader = get_node("../Fader")
 onready var fader_tween = get_node("../Fader/Tween")
 var pressing_disabled = false
+var start_pos
+var last_hovered = false
 
-export var bottom_texture_play : StreamTexture
-export var top_texture_play : StreamTexture
+export var texture_play : StreamTexture
+export var texture_stop : StreamTexture
 
-export var bottom_texture_stop : StreamTexture
-export var top_texture_stop : StreamTexture
+func _ready():
+	start_pos = self.rect_position
 
 func _process(delta):
 	if pressed:
@@ -22,32 +27,50 @@ func _process(delta):
 	else:
 		self.modulate = Color(1, 1, 1)
 		
+	if is_hovered() and !last_hovered:
+		hover_sound.play()
+		tween_hover.interpolate_property(self, "rect_pivot_offset",
+			Vector2(60, 300), Vector2(60, 290), 0.075,
+			Tween.TRANS_CIRC, Tween.EASE_OUT)
+		tween_hover.start()
+	if !is_hovered() and last_hovered:
+		tween_hover.interpolate_property(self, "rect_pivot_offset",
+			Vector2(60, 290), Vector2(60, 300), 0.075,
+			Tween.TRANS_CIRC, Tween.EASE_OUT)
+		tween_hover.start()
+	last_hovered = is_hovered()
+		
 func _input(event):
 	if event.is_action_pressed("switch_modes"):
 		_pressed()
 		
 func change_visuals(new_scene):
-	self.texture_normal = bottom_texture_stop if new_scene == "Player" else bottom_texture_play
-	top_part.texture = top_texture_stop if new_scene == "Player" else top_texture_play
+	self.texture_normal = texture_stop if new_scene == "Player" else texture_play
 
 func _pressed():
 	if !pressing_disabled:
+		sound.play()
 		pressing_disabled = true
-		rect_rotation = 0
-		top_part.rect_rotation = 0
+		rect_position = start_pos
 		
-		tween.interpolate_property(top_part, "rect_rotation",
-			0, -45, 0.35,
+		tween.interpolate_property(self, "rect_position",
+			start_pos, start_pos + Vector2(0, -15), 0.25,
 			Tween.TRANS_CIRC, Tween.EASE_OUT)
 		tween.start()
-		tween_bottom.interpolate_property(self, "rect_rotation",
-			0, -10, 0.35,
+		
+		tween_coin.interpolate_property(coin, "position",
+			Vector2(0, 0), Vector2(0, -200), 0.25,
 			Tween.TRANS_CIRC, Tween.EASE_OUT)
-		tween_bottom.start()
+		tween_coin.start()
+		
+		tween_disappear.interpolate_property(coin, "modulate",
+			Color(1, 1, 1, 1), Color(1, 1, 1, 0), 0.23,
+			Tween.TRANS_CIRC, Tween.EASE_IN)
+		tween_disappear.start()
 		
 		fader.visible = true
 		fader_tween.interpolate_property(fader, "modulate",
-			Color(1, 1, 1, 0), Color(1, 1, 1, 1), 0.35,
+			Color(1, 1, 1, 0), Color(1, 1, 1, 1), 0.20,
 			Tween.TRANS_LINEAR, Tween.EASE_OUT)
 		fader_tween.start()
 		
@@ -57,16 +80,11 @@ func _pressed():
 		var scene_path = "res://scenes/player/player.tscn" if get_tree().get_current_scene().get_name() == "Editor" else "res://scenes/editor/editor.tscn" 
 		get_tree().change_scene(scene_path)
 		change_visuals(new_scene)
-		sound.play()
 		
-		tween.interpolate_property(top_part, "rect_rotation",
-			-45, 0, 0.20,
-			Tween.TRANS_EXPO, Tween.EASE_OUT)
+		tween.interpolate_property(self, "rect_position",
+			start_pos + Vector2(0, -15), start_pos, 0.25,
+			Tween.TRANS_CIRC, Tween.EASE_OUT)
 		tween.start()
-		tween_bottom.interpolate_property(self, "rect_rotation",
-			-10, 0, 0.35,
-			Tween.TRANS_BACK, Tween.EASE_OUT)
-		tween_bottom.start()
 		
 		fader_tween.interpolate_property(fader, "modulate",
 			Color(1, 1, 1, 1), Color(1, 1, 1, 0), 0.20,
@@ -76,6 +94,5 @@ func _pressed():
 		yield(tween, "tween_completed")
 		
 		fader.visible = false
-		rect_rotation = 0
-		top_part.rect_rotation = 0
+		rect_position = start_pos
 		pressing_disabled = false
