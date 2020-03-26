256c6a9a8cb7ac221c3337408f5dfb60da1b82e5
SuperMakerPlayer <51249300+SuperMakerPlayer@users.noreply.github.com>
2020-03-26 16:53:58 +0100

    actually fix it

--- scenes/player/ip_setter.gd
+++ scenes/player/ip_setter.gd
@@ -1,8 +1,11 @@
 extends LineEdit
 	
-func focus_exited():
+func focus_exit():
 	PlayerSettings.connect_to_ip = text
 
 func _input(event):
 	if event.is_action_pressed("text_release_focus"): # this should already be a thing
 		release_focus()
+
+func _ready():
+	connect("focus_exited", self, "focus_exit")
