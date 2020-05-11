25959e54118e59d28f3de0925b39d19cb59ce73f
SuperMakerPlayer <51249300+SuperMakerPlayer@users.noreply.github.com>
2020-05-11 21:41:42 +0200

    change coin sync method

--- scenes/actors/objects/yellow_coin/coin.gd
+++ scenes/actors/objects/yellow_coin/coin.gd
@@ -1,13 +1,14 @@
 extends GameObject
 
 onready var animated_sprite = $AnimatedSprite
-onready var synced_sprite = $Sprite
 onready var sound = $AudioStreamPlayer
 onready var area = $Area2D
 onready var visibility_enabler = $VisibilityEnabler2D
 
 var collected = false
 var destroy_timer = 0.0
+
+export var anim_damp = 80
 
 func collect(body):
 	if enabled and !collected and body.name.begins_with("Character") and !body.dead:
@@ -17,16 +18,10 @@ func collect(body):
 			player_id = 0
 		if PlayerSettings.other_player_id == -1 or PlayerSettings.my_player_index == player_id:
 			sound.play()
-		collected = true;
-		# this shouldn't have to be a thing, but godot is annoying
-		synced_sprite.visible = false
-		animated_sprite.visible = true
-		animated_sprite.playing = true
+		collected = true
+		animated_sprite.animation = "collect"
 		animated_sprite.frame = 0
 		destroy_timer = 2
-		visibility_enabler.pause_animated_sprites = false
-		visibility_enabler.pause_animations = false
-		visibility_enabler.physics_process_parent = false
 		
 func _ready():
 	var _connect = area.connect("body_entered", self, "collect")
@@ -37,3 +32,5 @@ func _process(delta):
 		if destroy_timer <= 0:
 			destroy_timer = 0
 			queue_free()
+	if !collected:
+		animated_sprite.frame = (OS.get_ticks_msec() / anim_damp) % 4
