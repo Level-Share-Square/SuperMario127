6ec9b81d5314c42b3c2c95297afcfe81e5180fa8
No U <bene.danika@gmail.com>
2020-07-15 21:51:08 +0200

    Scale the trail - doesn't work correctly with non-uniform scales (e.g. 2,1), which is a Godot limitation

--- scenes/actors/objects/green_demon/green_demon.gd
+++ scenes/actors/objects/green_demon/green_demon.gd
@@ -34,6 +34,7 @@ func kill(body):
 		effects.visible = false
 
 func _ready():
+	particles.process_material.scale = (scale.x + scale.y) / 2
 	particles.amount = 6 * chase_speed
 	if mode != 1:
 		var _connect = area.connect("body_entered", self, "kill")
