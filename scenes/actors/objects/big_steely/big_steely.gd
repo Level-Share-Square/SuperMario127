ee5e0cbedc2eea935fb849e61730a90382079e4d
No U <bene.danika@gmail.com>
2020-07-16 10:49:53 +0200

    Tweaked big steely physics

--- scenes/actors/objects/big_steely/big_steely.gd
+++ scenes/actors/objects/big_steely/big_steely.gd
@@ -13,6 +13,7 @@ onready var collider_stopped = $KinematicBody2D/CollisionShapeStopped
 onready var visiblity_notifier = $KinematicBody2D/VisibilityNotifier2D
 
 var velocity := Vector2(0, 0)
+var prev_pos := Vector2(0, 0)
 var gravity : float
 
 func is_grounded():
@@ -22,6 +23,17 @@ func is_grounded():
 	return check.is_colliding()
 
 func _physics_process(delta):
+	# Use the position difference to calculate velocity
+	# (the one in the physics body isn't accurate
+	# for hitting Mario)
+	var new_pos = body.position
+	var actual_velocity = (new_pos - prev_pos) / delta / 120
+	prev_pos = new_pos
+	
+	var should_hit = actual_velocity.length_squared() > 0.09
+	if should_hit:
+		print(str(actual_velocity.length_squared()))
+	
 	var platform_collision_enabled = false
 	for platform_body in platform_detector.get_overlapping_areas():
 		if platform_body.has_method("is_platform_area"):
@@ -31,11 +43,12 @@ func _physics_process(delta):
 	grounded_check.set_collision_mask_bit(4, platform_collision_enabled)
 	grounded_check_2.set_collision_mask_bit(4, platform_collision_enabled)
 
-	for hit_body in area.get_overlapping_bodies():
-		if hit_body.has_method("steely_hit") and abs(velocity.x) > 30:
-			hit_body.steely_hit(global_position)
-		elif hit_body.get_parent().has_method("steely_hit") and abs(velocity.x) > 30:
-			hit_body.get_parent().steely_hit(global_position)
+	if should_hit:
+		for hit_body in area.get_overlapping_bodies():
+			if hit_body.has_method("steely_hit"):
+				hit_body.steely_hit(global_position)
+			elif hit_body.get_parent().has_method("steely_hit"):
+				hit_body.get_parent().steely_hit(global_position)
 
 	gravity = CurrentLevelData.level_data.areas[CurrentLevelData.area].settings.gravity
 	velocity.y += gravity
@@ -43,6 +56,15 @@ func _physics_process(delta):
 	var check = grounded_check
 	if !grounded_check.is_colliding():
 		check = grounded_check_2
+	
+	if !should_hit:# and abs(check.get_collision_normal().y) > 0.75:
+		body.set_collision_layer_bit(0, true)
+		collider.disabled = true
+		collider_stopped.disabled = false
+	else:
+		body.set_collision_layer_bit(0, false)
+		collider.disabled = false
+		collider_stopped.disabled = true
 		
 	if check.is_colliding():
 		if check.get_collision_normal().y == -1:
@@ -54,15 +76,6 @@ func _physics_process(delta):
 			else:
 				normal = 1 - check.get_collision_normal().x
 			velocity.x = lerp(velocity.x, 225 * normal, delta / 1.5)
-			
-	if abs(velocity.x) <= 30 and abs(check.get_collision_normal().y) > 0.75:
-		body.set_collision_layer_bit(0, true)
-		collider.disabled = true
-		collider_stopped.disabled = false
-	else:
-		body.set_collision_layer_bit(0, false)
-		collider.disabled = false
-		collider_stopped.disabled = true
 
 	rotation = 0
 	velocity = body.move_and_slide(velocity)
