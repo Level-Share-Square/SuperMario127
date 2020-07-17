3602d2367c1f3ac213590b359777cb921c9de18f
No U <bene.danika@gmail.com>
2020-07-17 21:45:26 +0200

    Fix auto-reduced window scales

--- scenes/player/window_scale.gd
+++ scenes/player/window_scale.gd
@@ -5,11 +5,19 @@ onready var right = $Right
 
 onready var value_text = $Value
 var window_scale = 1
+const PATH = "user://settings.json"
 const DEFAULT_SIZE = Vector2(768, 432)
 const WIDTH_FACTOR = DEFAULT_SIZE.x / DEFAULT_SIZE.y
 
 func _ready():
-	window_scale = 5 if OS.window_fullscreen else (OS.window_size.x / DEFAULT_SIZE.x)
+	var file = File.new()
+	file.open(PATH, File.READ)
+	
+	var data = parse_json(file.get_as_text())
+	
+	file.close()
+	
+	window_scale = 5 if OS.window_fullscreen else (OS.window_size.x / DEFAULT_SIZE.x) if data["windowScale"] == null else data["windowScale"]
 	value_text.text = str(window_scale) if window_scale != 5 else "F"
 	var _connect = left.connect("pressed", self, "decrease_value")
 	var _connect2 = right.connect("pressed", self, "increase_value")
@@ -30,8 +38,18 @@ func process():
 	OS.window_fullscreen = window_scale == 5
 	
 	var window_size : Vector2 = DEFAULT_SIZE * window_scale
-	if window_size.x > OS.get_screen_size().x:
-		OS.window_size = Vector2(OS.get_screen_size().x, OS.get_screen_size().y * WIDTH_FACTOR)
-	else:
-		OS.window_size = DEFAULT_SIZE * window_scale
+	print(window_size)
+	var max_size = Vector2(OS.get_screen_size().x, OS.get_screen_size().y)
+	print(max_size)
+	var modified = false
+	if window_size.x > max_size.x:
+		OS.window_size = Vector2(max_size.x, max_size.x / WIDTH_FACTOR)
+		modified = true
+	if window_size.y > max_size.y:
+		OS.window_size = Vector2(max_size.y * WIDTH_FACTOR, max_size.y)
+		modified = true
+	
+	if !modified:
+		OS.window_size = window_size
+	
 	value_text.text = str(window_scale) if window_scale != 5 else "F"
--- util/settingssaver_util.gd
+++ util/settingssaver_util.gd
@@ -1,6 +1,8 @@
 class_name SettingsSaver
 
 const PATH = "user://settings.json"
+const DEFAULT_SIZE = Vector2(768, 432)
+const WIDTH_FACTOR = DEFAULT_SIZE.x / DEFAULT_SIZE.y
 
 static func load():
 	var file = File.new()
@@ -12,9 +14,26 @@ static func load():
 	if typeof(data) == TYPE_DICTIONARY:
 		Engine.target_fps = 10 * (data["fpsLock"] + 3)
 		PlayerSettings.control_mode = data["controlMode"]
-		OS.window_fullscreen = data["windowScale"] == 5
-		OS.window_size = Vector2(768, 432) * data["windowScale"]
+		set_screen_size(data)
+
+static func set_screen_size(data):
+	OS.window_fullscreen = data["windowScale"] == 5
 	
+	var window_size : Vector2 = DEFAULT_SIZE * data["windowScale"]
+	print(window_size)
+	var max_size = Vector2(OS.get_screen_size().x, OS.get_screen_size().y)
+	print(max_size)
+	var modified = false
+	if window_size.x > max_size.x:
+		OS.window_size = Vector2(max_size.x, max_size.x / WIDTH_FACTOR)
+		modified = true
+	if window_size.y > max_size.y:
+		OS.window_size = Vector2(max_size.y * WIDTH_FACTOR, max_size.y)
+		modified = true
+	
+	if !modified:
+		OS.window_size = window_size
+
 static func save(multiplayerOptions : Node):
 	var controlModeLabel : Label = multiplayerOptions.get_node("ControlMode/Value")
 	var windowScaleLabel : Label = multiplayerOptions.get_node("WindowScale/Value")
