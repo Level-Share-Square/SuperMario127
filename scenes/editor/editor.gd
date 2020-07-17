5923e1a097de6b960bd728dc32e0bc7fdfec8719
No U <bene.danika@gmail.com>
2020-07-17 20:59:12 +0200

    Reduce 'if hovered_object and' statements

--- scenes/editor/editor.gd
+++ scenes/editor/editor.gd
@@ -165,49 +165,49 @@ func _process(delta):
 				hovered_object = null
 				item_preview_node.visible = true
 		
-		if hovered_object and Input.is_action_just_pressed("rotate"):
-			rotating = true
+		if hovered_object:
+			if Input.is_action_just_pressed("rotate"):
+				rotating = true
 			
-		if hovered_object and Input.is_action_just_pressed("flip_object"):
-			hovered_object.set_property("scale", Vector2(-hovered_object.scale.x, hovered_object.scale.y), true)
-		
-		if hovered_object and Input.is_action_just_pressed("flip_object_v"):
-			hovered_object.set_property("scale", Vector2(hovered_object.scale.x, -hovered_object.scale.y), true)
+			if Input.is_action_just_pressed("flip_object"):
+				hovered_object.set_property("scale", Vector2(-hovered_object.scale.x, hovered_object.scale.y), true)
 			
-		if hovered_object and left_held and selected_tool == 0 and Input.is_action_just_pressed("place") and !rotating:
-			time_clicked += delta
+			if Input.is_action_just_pressed("flip_object_v"):
+				hovered_object.set_property("scale", Vector2(hovered_object.scale.x, -hovered_object.scale.y), true)
 			
-		if hovered_object and time_clicked > 0 and left_held:
-			time_clicked += delta
-			if time_clicked > 0.2:
-				var obj_position = mouse_pos
+			if left_held and selected_tool == 0 and Input.is_action_just_pressed("place") and !rotating:
+				time_clicked += delta
+			
+			if time_clicked > 0 and left_held:
+				time_clicked += delta
+				if time_clicked > 0.2:
+					var obj_position = mouse_pos
+					if !Input.is_action_pressed("8_pixel_lock"):
+						obj_position = Vector2(stepify(obj_position.x, 8), stepify(obj_position.y, 8))
+					hovered_object.set_property("position", obj_position, true)
+			
+			if rotating:
+				hovered_object.rotation = -90 + hovered_object.position.angle_to_point(mouse_pos)
 				if !Input.is_action_pressed("8_pixel_lock"):
-					obj_position = Vector2(stepify(obj_position.x, 8), stepify(obj_position.y, 8))
-				hovered_object.set_property("position", obj_position, true)
-
-		if hovered_object and rotating:
-			hovered_object.rotation = -90 + hovered_object.position.angle_to_point(mouse_pos)
-			if !Input.is_action_pressed("8_pixel_lock"):
-				hovered_object.rotation_degrees = stepify(hovered_object.rotation_degrees, 15)
-
-		if hovered_object and Input.is_action_just_released("place") and time_clicked > 0 and time_clicked < 0.2:
-			if !rotating:
-				object_settings_node.open_object(hovered_object)
-
-		if hovered_object and Input.is_action_just_pressed("place") and rotating:
-			rotating = false
-			hovered_object.set_property("rotation_degrees", fmod(hovered_object.rotation_degrees, 360), true)
-
+					hovered_object.rotation_degrees = stepify(hovered_object.rotation_degrees, 15)
+			
+			if Input.is_action_just_released("place") and time_clicked > 0 and time_clicked < 0.2:
+				if !rotating:
+					object_settings_node.open_object(hovered_object)
+			
+			if Input.is_action_just_pressed("place") and rotating:
+				rotating = false
+				hovered_object.set_property("rotation_degrees", fmod(hovered_object.rotation_degrees, 360), true)
+		
 		if !left_held:
 			time_clicked = 0
-
-		if !left_held and last_left_held or !right_held and last_right_held:
-			var action = Action.new()
-			action.type = "place_tile"
-			for element in tiles_stack:
-				action.data.append(element)
-			tiles_stack.clear()
-			ActionManager.add_action(action)
+			if last_left_held or !right_held and last_right_held:
+				var action = Action.new()
+				action.type = "place_tile"
+				for element in tiles_stack:
+					action.data.append(element)
+				tiles_stack.clear()
+				ActionManager.add_action(action)
 
 		if (left_held and selected_tool == 0) and selected_box and selected_box.item:
 			var item = selected_box.item
