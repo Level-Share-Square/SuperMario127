10da330b8c66207c24892b6e7baea20924f9d056
fluentcoding <zsuckylp@gmail.com>
2020-07-17 21:25:58 +0200

    Fixed window size getting bigger than screen size
    The window size cannot get bigger than the screen anymore by locking it to the screen size.

--- scenes/player/window_scale.gd
+++ scenes/player/window_scale.gd
@@ -5,9 +5,11 @@ onready var right = $Right
 
 onready var value_text = $Value
 var window_scale = 1
+const DEFAULT_SIZE = Vector2(768, 432)
+const WIDTH_FACTOR = DEFAULT_SIZE.x / DEFAULT_SIZE.y
 
 func _ready():
-	window_scale = 5 if OS.window_fullscreen else (OS.window_size.x / 768)
+	window_scale = 5 if OS.window_fullscreen else (OS.window_size.x / DEFAULT_SIZE.x)
 	value_text.text = str(window_scale) if window_scale != 5 else "F"
 	var _connect = left.connect("pressed", self, "decrease_value")
 	var _connect2 = right.connect("pressed", self, "increase_value")
@@ -26,5 +28,10 @@ func increase_value():
 
 func process():
 	OS.window_fullscreen = window_scale == 5
-	OS.window_size = Vector2(768, 432) * window_scale
+	
+	var window_size : Vector2 = DEFAULT_SIZE * window_scale
+	if window_size.x > OS.get_screen_size().x:
+		OS.window_size = Vector2(OS.get_screen_size().x, OS.get_screen_size().y * WIDTH_FACTOR)
+	else:
+		OS.window_size = DEFAULT_SIZE * window_scale
 	value_text.text = str(window_scale) if window_scale != 5 else "F"
