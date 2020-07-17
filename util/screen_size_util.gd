96ed49b41bdb81a8e70c04b3e2d93884800b3de7
No U <bene.danika@gmail.com>
2020-07-17 22:02:15 +0200

    Remove print lol

--- util/screen_size_util.gd
+++ util/screen_size_util.gd
@@ -7,9 +7,8 @@ static func set_screen_size(window_scale):
 	OS.window_fullscreen = window_scale == 5
 	
 	var window_size : Vector2 = DEFAULT_SIZE * window_scale
-	print(window_size)
 	var max_size = Vector2(OS.get_screen_size().x, OS.get_screen_size().y)
-	print(max_size)
+	
 	var modified = false
 	if window_size.x > max_size.x:
 		OS.window_size = Vector2(max_size.x, max_size.x / WIDTH_FACTOR)
