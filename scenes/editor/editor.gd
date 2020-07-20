141e3a68c65169d743769d1655c98ba9695226ab
NoThisIsStupider <joshuaeagles1@gmail.com>
2020-07-20 01:25:22 -0400

    oops, another regression you could spam the mode switch button since the state was reset on editor ready always now it's only reset the first time the editor is made ready

--- scenes/editor/editor.gd
+++ scenes/editor/editor.gd
@@ -111,9 +111,9 @@ func _ready():
 	layers_transparent = EditorSavedSettings.layers_transparent
 	shared_node.toggle_layer_transparency(layer, layers_transparent)
 
-	if !get_node("/root/music").is_playing():
+	if get_node("/root/mode_switcher/ModeSwitcherButton").invisible: #so basically, if the mode switch button is invisible then the editor hasn't been initialized for the first time yet, since it isn't made invisible between mode switches for edit and play, hopefully this doesn't cause issues later on
+		get_node("/root/mode_switcher/ModeSwitcherButton").change_button_state(true) #enable the mode switching button since we're using the editor
 		get_node("/root/music").play() #needed because the music no longer plays by default
-	get_node("/root/mode_switcher/ModeSwitcherButton").change_button_state(true) #enable the mode switching button since we're using the editor
 	
 func set_selected_box(new_selected_box: Node):
 	EditorSavedSettings.selected_box = new_selected_box.box_index
