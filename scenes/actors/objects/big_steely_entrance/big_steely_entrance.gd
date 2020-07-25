bdf0465ca4baaf214693f625bbbab73ef722d565
NoThisIsStupider <joshuaeagles1@gmail.com>
2020-07-25 09:11:41 -0400

    prevent obstructed steelie spawner from spawning

--- scenes/actors/objects/big_steely_entrance/big_steely_entrance.gd
+++ scenes/actors/objects/big_steely_entrance/big_steely_entrance.gd
@@ -16,12 +16,19 @@ func _set_property_values():
 func _ready():
 	cleanout_timer = 10.0
 	
-func _process(delta):
+func _physics_process(delta):
 	if mode != 1:
 		spawn_timer -= delta
 		if spawn_timer <= 0:
 			spawn_timer = spawn_interval
-			if true: #steely_objects.size() <= 16:
+			
+			#use the collision shape to query the space state to check for collisions with steelies, if there's any steelies in the way don't spawn a new one
+			var shape_query_parameters = Physics2DShapeQueryParameters.new()
+			shape_query_parameters.set_shape($CollisionShape2D.shape)
+			shape_query_parameters.transform = transform
+			shape_query_parameters.collision_layer = 32
+			var no_steelies_in_front = get_world_2d().direct_space_state.intersect_shape(shape_query_parameters).empty()
+			if no_steelies_in_front: #steely_objects.size() <= 16: this is some old code, was commented out before adding the no_steelies_in_front stuff
 				var object = LevelObject.new()
 				object.type_id = 37
 				object.properties = []
