661b7b8aa2004e0ba1235bc8457273ae3f6aca63
jupahe64 <33004544+jupahe64@users.noreply.github.com>
2020-08-03 00:19:39 +0200

    simplify cloud_platforms setup and add resizing to seesaw

--- scenes/actors/objects/cloud_platform/cloud_platform.gd
+++ scenes/actors/objects/cloud_platform/cloud_platform.gd
@@ -5,73 +5,61 @@ onready var area_collision_shape = $Area2D/CollisionShape2D
 onready var collision_shape = $StaticBody2D/CollisionShape2D
 onready var body = $StaticBody2D
 
-onready var parts_holder = $Parts
-onready var left = $LeftSprite
-onready var right = $RightSprite
+onready var sprite = $Sprite
 
 export var middle_texture : StreamTexture
 
 export var parts := 1
 var buffer := -5
-export var left_width = 22
-export var right_width = 24
-export var base_collision_width = 13
-export var part_width = 19
+onready var left_width = sprite.patch_margin_left
+onready var right_width = sprite.patch_margin_right
+onready var part_width = sprite.texture.get_width() - left_width - right_width
 
 func _ready():
+	#make shapes unique so we don't accidently modify them for all platforms
+	area_collision_shape.shape = area_collision_shape.shape.duplicate(true)
+	collision_shape.shape = collision_shape.shape.duplicate(true)
+	
 	if !get_parent().enabled:
 		collision_shape.disabled = true
-#	if get_parent().mode != 1:   this seems to be unused
-#		var _connect = area.connect("body_entered", self, "enter_area")
-#		var _connect2 = area.connect("body_exited", self, "exit_area")
-		
+	
+	collision_shape.one_way_collision = false
+	
 func update_parts():
-	for part_sprite in parts_holder.get_children():
-		part_sprite.queue_free()
-	
-	for index in range(parts):
-		var part = Sprite.new()
-		part.centered = false
-		part.texture = middle_texture
-		part.position.x = left_width + (part_width * index)
-		part.position.y = -8
-		parts_holder.add_child(part)
-	right.position.x = left_width + (part_width * parts)
-	
-	var area_shape = RectangleShape2D.new()
-	area_shape.extents.x = (left_width + (part_width * parts) + right_width) / 2
-	area_shape.extents.y = 32
-	area_collision_shape.position.x = (left_width + (part_width * parts) + right_width) / 2
-	area_collision_shape.shape = area_shape
-	
-	var rect_shape = RectangleShape2D.new()
-	rect_shape.extents.x = (left_width + (part_width * parts) + right_width) / 2
-	rect_shape.extents.y = 5
-	collision_shape.position.x = (left_width + (part_width * parts) + right_width) / 2
-	collision_shape.shape = rect_shape
-	
-	position.x = -(left_width + (part_width * parts) + right_width) / 2
+	sprite.rect_position.x = -(left_width + (part_width * parts) + right_width) / 2
+	sprite.rect_size.x = left_width + right_width + part_width * parts
+
+	area_collision_shape.shape.extents.x = (left_width + (part_width * parts) + right_width) / 2 + 20
+	collision_shape.shape.extents.x = (left_width + (part_width * parts) + right_width) / 2
 	
 func can_collide_with(character):
 	var direction = body.global_transform.y.normalized()
 	
-	var is_grounded = character.is_grounded() if character.has_method("is_grounded") else true
+	# Use prev_is_grounded because calling is_grounded() is broken
+	var is_grounded = character.prev_is_grounded if character.get("prev_is_grounded") != null else true
+	# Some math that gives us useful vectors
 	var line_center = body.global_position + (direction * buffer)
 	var line_direction = Vector2(-direction.y, direction.x)
 	var p1 = line_center + line_direction
 	var p2 = line_center - line_direction
-	var p = character.bottom_pos.global_position if is_grounded else character.global_position
+	var p = character.bottom_pos.global_position #if is_grounded else character.global_position
+	#var velocity = character.velocity if character.get("velocity") != null else Vector2(0, 0) seems to be unused, uncomment if needed
 	var diff = p2 - p1
 	var perp = Vector2(-diff.y, diff.x)
-	# A threshold that should prevent clips
-	if character.get("velocity") != null and !is_grounded:
+	
+	if !is_grounded:
+		# If in the air, check for the velocity first
+		# If we're trying to pass through it from the other way around,
+		# cancel it
 		var d = character.velocity.dot(perp)
 		if d < 0:
 			return false
 		
+		# In both cases, a threshold is applied that prevents clips
 		p -= character.velocity.normalized()
 	else:
 		p -= perp
 	
+	# Is p on the correct side?
 	var d = (p - p1).dot(perp)
 	return sign(d) != 1
--- scenes/actors/objects/seesaw/seesaw.gd
+++ scenes/actors/objects/seesaw/seesaw.gd
@@ -1,7 +1,48 @@
 extends GameObject
 
+
+#-------------------------------- GameObject logic -----------------------
+
+var parts := 1
+var last_parts := 1
+
+func _set_properties():
+	savable_properties = ["parts"]
+	editable_properties = ["parts"]
+	
+func _set_property_values():
+	set_property("parts", parts, 1)
+
+func _input(event):
+	if event is InputEventMouseButton and event.is_pressed() and hovered:
+		if event.button_index == 5: # Mouse wheel down
+			parts -= 1
+			if parts < 0:
+				parts = 0
+			set_property("parts", parts, true)
+		elif event.button_index == 4: # Mouse wheel up
+			parts += 1
+			set_property("parts", parts, true)
+
+func _process(_delta):
+	if parts != last_parts:
+		update_parts()
+	last_parts = parts
+
+
+
+
+
+
+#-------------------------------- platform logic -----------------------
+
 onready var body = $KinematicBody2D
+onready var sprite = $KinematicBody2D/Sprite
 onready var area = $KinematicBody2D/Area2D
+
+onready var platform_area_collision_shape = $PlatformArea/CollisionShape2D
+onready var area_collision_shape = $KinematicBody2D/CollisionShape2D
+onready var collision_shape = $KinematicBody2D/Area2D/CollisionShape2D
 
 var buffer := -5
 
@@ -11,14 +52,33 @@ var rotation_speed := 0.0
 
 const HALF_PI = PI/2
 
+onready var left_width = sprite.patch_margin_left
+onready var right_width = sprite.patch_margin_right
+onready var part_width = sprite.texture.get_width() - left_width - right_width
+
 func _ready():
-	pass
+	platform_area_collision_shape.shape = platform_area_collision_shape.shape.duplicate(true)
+	area_collision_shape.shape = area_collision_shape.shape.duplicate(true)
+	collision_shape.shape = collision_shape.shape.duplicate(true)
+	
+	if !enabled:
+		collision_shape.disabled = true
+		
+	update_parts()
+		
+func update_parts():
+	sprite.rect_position.x = -(left_width + (part_width * parts) + right_width) / 2
+	sprite.rect_size.x = left_width + right_width + part_width * parts
+
+	platform_area_collision_shape.shape.extents.x = (left_width + (part_width * parts) + right_width) / 2 + 20
+	area_collision_shape.shape.extents.x = (left_width + (part_width * parts) + right_width) / 2
+	collision_shape.shape.extents.x = (left_width + (part_width * parts) + right_width) / 2
 
 func _physics_process(delta):
 	if(mode==1):
 		return
 	
-	var scale_x = scale.x
+	var scale_x = scale.x * (left_width + right_width + part_width * parts) / 64
 	
 	var delta_rotation := 0
 	for _body in area.get_overlapping_bodies():
