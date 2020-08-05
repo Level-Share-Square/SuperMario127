b4b6d50705c7e4b0570feaea6cd53316988f0a86
SuperMakerPlayer <51249300+SuperMakerPlayer@users.noreply.github.com>
2020-08-05 15:48:34 -0700

    fix powerup music playing after scene reloads

--- scenes/actors/scene_transitions/transition_script.gd
+++ scenes/actors/scene_transitions/transition_script.gd
@@ -38,6 +38,7 @@ func reload_scene(transition_in_tex, transition_out_tex, transition_time, new_ar
 
 func do_transition_animation(transition_texture, transition_time, texture_scale_start, texture_scale_end, volume_start, volume_end, reverse_after = false):
 	tween.stop_all()
+	music.stop_temporary_music()
 		
 	canvas_background.visible = true
 		
@@ -46,7 +47,7 @@ func do_transition_animation(transition_texture, transition_time, texture_scale_
 	# if the start scale is greater, then the screen is transitioning to black
 	var to_black = texture_scale_start > texture_scale_end
 	tween.interpolate_property(canvas_mask, "texture_scale", texture_scale_start, texture_scale_end, transition_time, Tween.TRANS_CIRC, Tween.EASE_OUT if to_black else Tween.EASE_IN)
-	if !music_node.is_tween_active(): #this is so it doesn't interfere with any temp music fade out, like the shine
+	if !music_node.is_tween_active() and music_node.volume_multiplier != 0: #this is so it doesn't interfere with any temp music fade out, like the shine
 		tween.interpolate_property(music_node, "volume_multiplier", volume_start, volume_end, transition_time, Tween.TRANS_CIRC, Tween.EASE_OUT)
 	tween.start()
 
