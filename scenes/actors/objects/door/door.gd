30377dad70ce4ac70fdf995d15ce7c0f530c9bc9
SuperMakerPlayer <51249300+SuperMakerPlayer@users.noreply.github.com>
2020-08-06 13:02:23 -0700

    add door exiting, and change a few backgrounds

--- scenes/actors/objects/door/door.gd
+++ scenes/actors/objects/door/door.gd
@@ -1,3 +1,13 @@
 extends GameObject
 
-onready var sprite = $DoorSprite
+onready var sprite = $DoorEnterLogic/DoorSprite
+onready var door_enter_logic = $DoorEnterLogic
+var stored_character : Character
+
+func _ready():
+	door_enter_logic.connect("start_door_logic", self, "_start_teleport")
+	
+func _start_teleport(character : Character):
+	# meant to start a scene transition and teleport mario to a different door, but for now just exits him out of the same door for testing
+	character.position = self.global_position
+	door_enter_logic.start_door_exit_animation(character)
