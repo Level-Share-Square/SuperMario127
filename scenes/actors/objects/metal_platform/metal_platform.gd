c4d5d4eebc565239a3715d97d6d07beeb253d66c
SuperMakerPlayer <51249300+SuperMakerPlayer@users.noreply.github.com>
2020-08-08 15:34:18 -0700

    change metal platform texture, make metal platform rescalable

---
+++ scenes/actors/objects/metal_platform/metal_platform.gd
@@ -0,0 +1,64 @@
+extends GameObject
+
+
+#-------------------------------- GameObject logic -----------------------
+
+var parts := 9
+var last_parts := 9
+
+func _set_properties():
+	savable_properties = ["parts"]
+	editable_properties = ["parts"]
+	
+func _set_property_values():
+	set_property("parts", parts, 1)
+
+func _input(event):
+	if event is InputEventMouseButton and event.is_pressed() and hovered:
+		if event.button_index == 5: # Mouse wheel down
+			parts -= 1
+			if parts < 1:
+				parts = 1
+			set_property("parts", parts, true)
+		elif event.button_index == 4: # Mouse wheel up
+			parts += 1
+			set_property("parts", parts, true)
+
+func _process(_delta):
+	if parts != last_parts:
+		update_parts()
+	last_parts = parts
+
+
+
+#-------------------------------- platform logic -----------------------
+	
+onready var sprite = $Sprite
+onready var platform_area_collision_shape = $StaticBody2D/Area2D/CollisionShape2D
+onready var collision_shape = $StaticBody2D/CollisionShape2D
+
+onready var left_width = sprite.patch_margin_left
+onready var right_width = sprite.patch_margin_right
+onready var part_width = sprite.texture.get_width() - left_width - right_width
+
+var scale_x : float
+
+func _ready():
+	platform_area_collision_shape.shape = platform_area_collision_shape.shape.duplicate(true)
+	collision_shape.shape = collision_shape.shape.duplicate(true)
+	
+	if !enabled:
+		collision_shape.disabled = true
+		platform_area_collision_shape.disabled = true
+		
+	update_parts()
+
+func update_parts():
+	sprite.rect_position.x = -(left_width + (part_width * parts) + right_width) / 2
+	sprite.rect_size.x = left_width + right_width + part_width * parts
+
+	platform_area_collision_shape.shape.extents.x = (left_width + (part_width * parts) + right_width) / 2 + 20
+	collision_shape.shape.extents.x = (left_width + (part_width * parts) + right_width) / 2
+	
+	#calculate the total platform scale
+	scale_x = scale.x * (left_width + right_width + part_width * parts) / (left_width + right_width + part_width)
