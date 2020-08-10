3ce3a890600733e1ae86c14db9697e72d342ad09
No U <bene.danika@gmail.com>
2020-08-10 23:08:33 +0200

    Temporary music no longer restarts & minor changes

--- scenes/actors/music/music.gd
+++ scenes/actors/music/music.gd
@@ -1,53 +1,53 @@
 extends AudioStreamPlayer
 
-export var play_bus: String
-export var edit_bus: String
+export var play_bus : String
+export var edit_bus : String
 
-var character = null
-var character2 = null
+var character : Character
+var character2 : Character
 
-onready var temporary_music_player = $TemporaryMusicPlayer
-onready var tween = $Tween
+onready var temporary_music_player : AudioStreamPlayer = $TemporaryMusicPlayer
+onready var tween : Tween = $Tween
 
-export var volume_multiplier : float = 1
-export var loading = false
-var base_volume = 0
-var stored_volume #used when playing temporary music to return the volume afterwards
+export var volume_multiplier := 1.0
+export var loading := false
+var base_volume := 0
+var stored_volume : float # used when playing temporary music to return the volume afterwards
 
-var last_mode = 3
-var last_song = 0
+var last_mode := 3
+var last_song := 0
 
-var dl_ready = false
-var downloader = Downloader.new()
+var dl_ready := false
+var downloader := Downloader.new()
 
-var song_cache = []
-var loop = 0.0
+var song_cache := []
+var loop := 0.0
 
-var muted = false
+var muted := false
 
 const MUSIC_FADE_LENGTH = 0.75
 
-func get_song(song_id: int):
+func get_song(song_id : int):
 	return song_cache[song_id]
 
-func _ready():
+func _ready() -> void:
 	base_volume = volume_db
 	var _connect = downloader.connect("request_completed", self, "load_ogg")
 	var level_songs : IdMap  = preload("res://assets/music/ids.tres")
 	for id in level_songs.ids:
 		song_cache.append(load("res://assets/music/resources/" + id + ".tres"))
-
-	temporary_music_player.connect("finished", self, "stop_temporary_music")
 	
-func is_tween_active():
+	temporary_music_player.connect("finished", self, "stop_temporary_music")
+
+func is_tween_active() -> bool:
 	return tween.is_active()
 	
-func load_ogg():
-	var path = "user://bg_music.ogg"
-	var ogg_file = File.new()
+func load_ogg() -> void:
+	var path := "user://bg_music.ogg"
+	var ogg_file := File.new()
 	ogg_file.open(path, File.READ)
-	var bytes = ogg_file.get_buffer(ogg_file.get_len())
-	var stream = AudioStreamOGGVorbis.new()
+	var bytes := ogg_file.get_buffer(ogg_file.get_len())
+	var stream := AudioStreamOGGVorbis.new()
 	stream.data = bytes
 	stream.loop = true
 	stream.loop_offset = loop
@@ -57,8 +57,8 @@ func load_ogg():
 	self.stream = stream
 	play()
 	print("Audio Loaded!")
-	
-func change_song(old_setting, music_setting):
+
+func change_song(old_setting, music_setting) -> void:
 	var song
 	
 	if typeof(music_setting) == TYPE_INT:
@@ -68,20 +68,17 @@ func change_song(old_setting, music_setting):
 			loop = 0.0
 			if music_setting.begins_with("LP"):
 				loop = float(music_setting.trim_prefix("LP").split("=")[0])
-				print(str(loop))
+				#print(str(loop))
 			stop()
 			downloader.download(music_setting, "user://", "bg_music.ogg")
 	
-	if song != null:
-		if stream != song.stream:
-			stream = song.stream
-			play()
-	if get_tree().get_current_scene().mode == 0:
-		bus = play_bus
-	else:
-		bus = edit_bus
+	if song != null and stream != song.stream:
+		stream = song.stream
+		play()
+	
+	bus = play_bus if get_tree().get_current_scene().mode == 0 else edit_bus
 
-func _process(_delta):
+func _process(_delta) -> void:
 	var current_scene = get_tree().get_current_scene()
 	var current_song = CurrentLevelData.level_data.areas[CurrentLevelData.area].settings.music
 	
@@ -96,28 +93,30 @@ func _process(_delta):
 	
 		volume_db = linear2db(db2linear(base_volume) * volume_multiplier) if !muted else -80.0
 
-func _unhandled_input(event):
+func _unhandled_input(event) -> void:
 	if event.is_action_pressed("mute"):
 		muted = !muted
 
 # the plan for this is to mute the current bgm, play the temp song, and then fade the current bgm back in
-func play_temporary_music(temp_song_id : int = 0, temp_song_volume : float = 0):
+func play_temporary_music(temp_song_id : int = 0, temp_song_volume : float = 0) -> void:
 	volume_multiplier = 0
 
 	tween.stop_all()
-	temporary_music_player.stream = get_song(temp_song_id).stream
-	temporary_music_player.volume_db = temp_song_volume if !muted else -80.0
-	temporary_music_player.play()
+	var stream = get_song(temp_song_id).stream
+	if temporary_music_player.stream != stream or !is_temporary_music_playing():
+		temporary_music_player.stream = stream
+		temporary_music_player.volume_db = temp_song_volume if !muted else -80.0
+		temporary_music_player.play()
 
 # returns the id of the temporary song
-func is_temporary_music_playing():
+func is_temporary_music_playing() -> bool:
 	return temporary_music_player.playing
 
-#can be called manually, also automatically called if the temporary music ends
-func stop_temporary_music(volume_multipler_target = 1, music_fade_length = MUSIC_FADE_LENGTH):
+# can be called manually, also automatically called if the temporary music ends
+func stop_temporary_music(volume_multiplier_target = 1, music_fade_length = MUSIC_FADE_LENGTH) -> void:
 	tween.interpolate_property(temporary_music_player, "volume_db", null, -80, music_fade_length, \
 			Tween.TRANS_LINEAR, Tween.EASE_IN)
 	tween.interpolate_callback(temporary_music_player, MUSIC_FADE_LENGTH, "stop")
-	tween.interpolate_property(self, "volume_multiplier", null, volume_multipler_target, music_fade_length, \
+	tween.interpolate_property(self, "volume_multiplier", null, volume_multiplier_target, music_fade_length, \
 			Tween.TRANS_LINEAR, Tween.EASE_IN)
 	tween.start()
