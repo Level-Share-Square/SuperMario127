8412c997dde67784d122e61bbddadfe5c189b918
NoThisIsStupider <joshuaeagles1@gmail.com>
2020-08-11 18:00:54 -0400

    Make photo mode not start on literally every input

--- scenes/player/photo_mode.gd
+++ scenes/player/photo_mode.gd
@@ -6,7 +6,7 @@ func _input(event):
 	var character2_node 
 	if PlayerSettings.number_of_players != 1:
 		character2_node = player.get_node(player.character2)
-	if !scene_transitions.transitioning and !mode_switcher.get_node("ModeSwitcherButton").switching_disabled or mode_switcher.get_node("ModeSwitcherButton").invisible and event.is_action_pressed("30_fps") and !(get_tree().paused and !PhotoMode.enabled):
+	if !scene_transitions.transitioning and (!mode_switcher.get_node("ModeSwitcherButton").switching_disabled or mode_switcher.get_node("ModeSwitcherButton").invisible) and event.is_action_pressed("30_fps") and !(get_tree().paused and !PhotoMode.enabled):
 		if !(character_node.dead or (PlayerSettings.number_of_players != 1 and character2_node.dead)):
 			PhotoMode.enabled = !PhotoMode.enabled
 			update_photo_mode()
