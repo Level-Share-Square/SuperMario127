081435d90438cb24e407e1f253b8131d252004c6
jupahe64 <33004544+jupahe64@users.noreply.github.com>
2020-08-12 02:59:14 +0200

    add move_type and touch_start properties to TouchLift and make it move framerate independent

--- scenes/actors/objects/touch_lift/touch_lift.gd
+++ scenes/actors/objects/touch_lift/touch_lift.gd
@@ -6,6 +6,15 @@ extends GameObject
 var parts := 4
 var last_parts := 4
 
+const MT_BACK_FORTH = 0
+const MT_RESET = 1
+const MT_ONCE = 2
+const MT_LOOP = 3
+
+
+var move_type := MT_BACK_FORTH
+var touch_start := false
+
 var end_position : Vector2
 var last_end_position : Vector2
 
@@ -14,14 +23,16 @@ var max_speed := 1.0
 var curve = null
 
 func _set_properties():
-	savable_properties = ["parts", "max_speed", "curve"]
-	editable_properties = ["parts", "max_speed", "end_position"]
+	savable_properties = ["parts", "max_speed", "curve", "move_type", "touch_start"]
+	editable_properties = ["parts", "max_speed", "end_position", "move_type", "touch_start"]
 	
 func _set_property_values():
 	set_property("parts", parts)
 	set_property("max_speed", max_speed)
 	set_property("curve", curve)
 	set_property("end_position", end_position)
+	set_property("move_type", move_type)
+	set_property("touch_start", touch_start)
 
 func _input(event):
 	if event is InputEventMouseButton and event.is_pressed() and hovered:
@@ -54,17 +65,20 @@ func _process(_delta):
 		last_end_position = end_position
 
 
-
 #-------------------------------- platform logic -----------------------
 
 onready var platform = $TouchLiftPlatform
+onready var platform_touch_area = $TouchLiftPlatform/FloorTouchArea
 onready var path_follower = $Path2D/PathFollow2D
 onready var path = $Path2D
 onready var platform_sprite = $TouchLiftPlatform/Sprite
 
 var speed := 1.0
-var virtual_offset := 0.0
+var loop_offset := 0.0
+var linear_offset := 0.0
 var time_alive = 0
+
+var activated = false
 
 onready var start_sprite_node : Node2D
 onready var end_sprite_node : Node2D
@@ -77,6 +91,10 @@ onready var right_width = platform_sprite.patch_margin_right
 onready var part_width = platform_sprite.texture.get_width() - left_width - right_width
 
 func _ready():
+	activated = !touch_start
+	
+	platform_touch_area.connect("body_entered", self, "_on_touch_area_entered")
+	
 	if curve==null and path.curve==null:
 		path.curve = Curve2D.new()
 		path.curve.add_point(Vector2())
@@ -113,20 +131,56 @@ func _draw():
 	if(mode==1):
 		draw_polyline(path.curve.get_baked_points(), line_color, 2.0)
 
-func _physics_process(_delta):
-	#var desired_speed = 0
+func _physics_process(delta):
+	if(!activated):
+		return
 	
-	virtual_offset += speed*max_speed
-	path_follower.offset = path_follower.offset * 0.95 + clamp(virtual_offset, 0.0, path.curve.get_baked_length())*0.05
+	linear_offset += speed * max_speed * 120 * delta
+	
+	if(move_type!=MT_LOOP):
+		linear_offset = clamp(linear_offset, 0.0, path.curve.get_baked_length()-0.01) #so the 
+	
+	var blend = pow(0.95, 120*delta)
+	
+	loop_offset = loop_offset * blend + linear_offset* (1-blend)
+	
+	path_follower.offset = fmod(loop_offset, path.curve.get_baked_length())
+	
+	if speed<0.0 and path_follower.offset<=2.0:
+		linear_offset = 0.0
+		speed = -speed
 
-	if(path_follower.offset<=2.0 and speed<0.0): #platform reached an end
-		speed = -speed
-		virtual_offset = 0.0
-	elif(path_follower.offset>=path.curve.get_baked_length()-2.0 and speed>0.0):
-		speed = -speed
-		virtual_offset = path.curve.get_baked_length()
+	elif move_type!=MT_LOOP and speed>0.0 and path_follower.offset>=path.curve.get_baked_length()-2.0:
+		linear_offset = path.curve.get_baked_length()
+		reached_end()
+
+		if(!activated):
+			return
 		
 	if(mode!=1):
 		platform.set_position(path_follower.position)
 	else:
 		platform.position = path_follower.position
+		
+func reached_end() -> void:
+	if(move_type==MT_BACK_FORTH):
+		speed = -speed
+		return
+	elif(move_type==MT_RESET):
+		$AnimationPlayer.play("Reset")
+	elif(move_type==MT_ONCE):
+		activated = false
+		
+func _on_touch_area_entered(body):
+	if(body is Character):
+		activated = true
+
+func reset_platform():
+	linear_offset = 0.0
+	loop_offset = 0.0
+	path_follower.offset = 0.0
+	
+	platform.position = path_follower.position
+	
+	activated = !touch_start
+	
