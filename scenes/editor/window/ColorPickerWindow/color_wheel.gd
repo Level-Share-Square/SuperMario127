25fbd5d48db291e98c7ae5053a775e33bea6a887
fluentcoding <zsuckylp@gmail.com>
2020-08-13 18:39:01 +0200

    Color Picker!

---
+++ scenes/editor/window/ColorPickerWindow/color_wheel.gd
@@ -0,0 +1,49 @@
+extends Sprite
+
+var pressed = false
+
+onready var selector = $"HSV Color Selector"
+onready var gradient_selector = get_parent().get_node("Gradient")
+onready var new_color_preview = get_parent().get_node("ColorPreviews/NewColorPreview")
+onready var color_manager = $Color
+
+var property_node : Node
+
+func _input(event):
+	if !get_parent().get_parent().visible:
+		return
+	
+	var mouse_pos = get_local_mouse_position()
+	var normal_coordinates = (mouse_pos) / get_rect().size.x * 2
+	
+	if event is InputEventMouseButton && event.button_index == BUTTON_LEFT:
+		pressed = event.pressed && normal_coordinates.length() <= 1
+	
+	if !pressed:
+		return
+	
+	normal_coordinates = normal_coordinates.normalized() * min(normal_coordinates.length(), 1)	
+	mouse_pos = mouse_pos.normalized() * min(mouse_pos.length(), get_rect().size.x * 0.5)
+	
+	# Is in sprite - check if its in the circle
+	selector.position = mouse_pos
+	var color = Color.from_hsv(atan2(normal_coordinates.x, normal_coordinates.y) / (2*PI), normal_coordinates.length(), 1)
+	gradient_selector.modulate = color
+	new_color_preview.modulate = color
+	color_manager.set_value(color)
+	notify_property_manager()
+
+func update_value(color : Color):
+	var length := get_rect().size.x * color.s / 2
+	var angle := color.h*2*PI
+	selector.position = Vector2(sin(angle)*length, cos(angle)*length)
+	gradient_selector.modulate = color
+	new_color_preview.modulate = color
+	self_modulate = Color(color.v, color.v, color.v)
+	gradient_selector.set_brightness(color.v)
+	notify_property_manager()
+
+func notify_property_manager():
+	var color = color_manager.get_value()
+	property_node.get_node("Color/Button").modulate = color
+	property_node.update_value(color)
---
+++ scenes/editor/window/ColorPickerWindow/gradient.gd
@@ -0,0 +1,35 @@
+extends Sprite
+
+var pressed = false
+
+onready var whiteY = -(get_rect().size.y / 2)
+onready var blackY = -whiteY
+onready var gradient_selector = get_node("Gradient Selector")
+onready var color_wheel = get_parent().get_node("Wheel")
+onready var new_color_preview = get_parent().get_node("ColorPreviews/NewColorPreview")
+onready var color_manager = color_wheel.get_node("Color")
+
+func _input(event):
+	if !color_wheel.get_parent().get_parent().visible:
+		return
+	 
+	var is_in_rect = get_rect().has_point(get_local_mouse_position())
+	
+	if event is InputEventMouseButton && event.button_index == BUTTON_LEFT:
+		pressed = event.pressed && is_in_rect
+	
+	if !pressed:
+		return
+	
+	gradient_selector.position.y = clamp(get_local_mouse_position().y, whiteY, blackY)
+	var v = (gradient_selector.position.y - blackY) / (whiteY - blackY)
+	color_wheel.self_modulate = Color(v, v, v)
+	
+	var new_color = color_manager.get_value()
+	new_color.v = v
+	color_manager.set_value(new_color)
+	new_color_preview.modulate = new_color
+	color_wheel.notify_property_manager()
+
+func set_brightness(brightness):
+	gradient_selector.position.y = lerp(blackY, whiteY, brightness)
