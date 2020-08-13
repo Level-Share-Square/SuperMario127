e76c500ee47a93c9f70c5dab7a722402eca2c72b
SuperMakerPlayer <51249300+SuperMakerPlayer@users.noreply.github.com>
2020-08-13 18:59:27 +0200

    final wing cap attempt and make smaller windows possible

--- scenes/actors/mario/states/WingMario.gd
+++ scenes/actors/mario/states/WingMario.gd
@@ -3,12 +3,15 @@ extends State
 class_name WingMarioState
 
 export var gravity_modifier = 4
-export var max_speed = 260
+export var max_speed = 560
+export var momentum = 1
 export var accel = 1
 export var turn_speed = 2.1
 
 var rotation_down = 0
 var old_gravity_scale = 0
+
+var current_speed = 0
 
 func _ready():
 	priority = 11
@@ -23,12 +26,15 @@ func _start_check(_delta):
 	return false #character.state == character.get_state_node("DiveState")
 
 func _start(_delta):
-	old_gravity_scale = character.gravity_scale
-	character.gravity_scale = 0
+	current_speed = 0
 	character.velocity.y /= 5
 	rotation_down = 90
 
-func _update(_delta):
+func _update(delta):
+	# Here's my latest attempt at coding this thing, as you can see i kind of failed terribly.
+	# If you end up doing the code for this yourself, i'd recommend deleting most of this
+	# as it's not very useful for future attempts and hasn't been cleaned up or commented
+
 	if character.facing_direction == 1:
 		character.sprite.animation = "idleRight"
 	else:
@@ -39,17 +45,17 @@ func _update(_delta):
 	
 	var rotation_normal = Vector2(cos(rotation_down), sin(rotation_down))
 	
-	character.velocity = Vector2((0.5 - abs(rotation_normal.x)) * 90, accel * 2)
+	character.velocity.y += momentum / 80
+	character.velocity.x = (0.5 - abs(rotation_normal.x)) * 0
 	
-	if character.inputs[character.input_names.left][character.input_params.pressed]:
+	if character.inputs[character.input_names.left][0]:
 		rotation_down -= turn_speed
-	elif character.inputs[character.input_names.right][character.input_params.pressed]:
+	elif character.inputs[character.input_names.right][0]:
 		rotation_down += turn_speed
 	
-	accel = rotation_down - 90
-
-func _stop(_delta):
-	character.gravity_scale = old_gravity_scale
+	momentum = (rotation_down - 160) * 3
+	if momentum < 0:
+		momentum *= 1
 
 func _stop_check(_delta):
 	return character.is_grounded()
