993ad7094ce8a20c5577deee18aa6f16fe2e0d40
No U <bene.danika@gmail.com>
2020-08-27 21:35:41 +0200

    Star coins now turn blue when collected

--- scenes/actors/objects/star_coin/star_coin.gd
+++ scenes/actors/objects/star_coin/star_coin.gd
@@ -1,12 +1,19 @@
 extends GameObject
 
+export var normal_frames : SpriteFrames
+export var collected_frames : SpriteFrames
+export var normal_particles : StreamTexture
+export var collected_particles : StreamTexture
+
 onready var anim_sprite : AnimatedSprite = $AnimatedSprite
+onready var particles : Particles2D = $AnimatedSprite/Particles2D
 onready var area : Area2D = $Area2D
-onready var audio_player : AudioStreamPlayer = $AudioStreamPlayer 
+onready var audio_player : AudioStreamPlayer = $AudioStreamPlayer
 onready var animation_player : AnimationPlayer = $AnimationPlayer
 
 var id : int = 0
-var collected : bool = false
+var collected := false
+var is_blue := false
 
 func _set_properties():
 	savable_properties = ["id"]
@@ -18,7 +25,21 @@ func _set_property_values():
 func _ready() -> void:
 	var _connect = area.connect("body_entered", self, "collect")
 
+	if SavedLevels.selected_level != SavedLevels.NO_LEVEL:
+		var collected_star_coins = SavedLevels.levels[SavedLevels.selected_level].collected_star_coins
+		# Get the value, returning false if the key doesn't exist
+		is_blue = collected_star_coins.get(str(id), false)
+
+	update_color()
 	anim_sprite.play("default")
+
+func update_color():
+	if !is_blue:
+		anim_sprite.frames = normal_frames
+		particles.texture = normal_particles
+	else:
+		anim_sprite.frames = collected_frames
+		particles.texture = collected_particles
 
 func collect(body : PhysicsBody2D) -> void:
 	if enabled and !collected and (body is Character):
@@ -31,4 +52,4 @@ func collect(body : PhysicsBody2D) -> void:
 		var _connect = animation_player.connect("animation_finished", self, "queue_free")
 
 		audio_player.play()
-		
+
