9452b2b99b8d490c8ae79a8d5caf1bf31c1487aa
No U <bene.danika@gmail.com>
2020-10-04 15:45:28 +0200

    Timer is hidden in Photo Mode:tm:

--- scenes/actors/time_score/time_score.gd
+++ scenes/actors/time_score/time_score.gd
@@ -7,7 +7,7 @@ var shown = false
 
 func _process(_delta) -> void:
 	var current_scene = get_tree().get_current_scene()
-	if "mode" in current_scene:
+	if "mode" in current_scene and !PhotoMode.enabled:
 		label.visible = (shown and current_scene.mode == 0 and mode_switcher.get_node("ModeSwitcherButton").invisible)
 	else:
 		label.visible = false
