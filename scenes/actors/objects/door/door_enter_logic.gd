4983fdb70fdba479d04f4689866f69ff353c2e52
SuperMakerPlayer <51249300+SuperMakerPlayer@users.noreply.github.com>
2020-12-04 21:55:29 +0100

    stop mario from getting hit while entering doors

--- scenes/actors/objects/door/door_enter_logic.gd
+++ scenes/actors/objects/door/door_enter_logic.gd
@@ -54,6 +54,8 @@ func start_door_enter_animation(character : Character) -> void:
 	character.movable = false
 	character.velocity = Vector2.ZERO
 	character.sprite.rotation = 0
+	character.set_collision_layer_bit(1, false) # disable collisions w/ most things
+	character.set_inter_player_collision(false)
 	character.camera.smoothing_enabled = true # Re-enable camera smoothing
 	
 	character.sprite.animation = "enterDoor" + ("Right" if character.facing_direction == 1 else "Left")
@@ -119,6 +121,9 @@ func door_exit_anim_finished(_animation : String, character : Character) -> void
 	character.invulnerable = false 
 	character.controllable = true
 	character.movable = true
+	# undo collision changes 
+	character.set_collision_layer_bit(1, true)
+	character.set_inter_player_collision(true) 
 	
 	character.sprite.animation = "exitDoor" + ("Right" if character.facing_direction == 1 else "Left")
 	character.sprite.playing = true
