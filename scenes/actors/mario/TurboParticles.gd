106d9519dafe5d1618465f51f19f2ba141642958
SuperMakerPlayer <51249300+SuperMakerPlayer@users.noreply.github.com>
2020-12-23 12:49:35 -0800

    fix turbo rainbow glitch

--- scenes/actors/mario/TurboParticles.gd
+++ scenes/actors/mario/TurboParticles.gd
@@ -1,8 +1,11 @@
 extends Particles2D
 
-onready var character : Character = $"../../"
+onready var character : Character
 onready var particle_material : ParticlesMaterial = process_material
 
+func _ready():
+	if get_node("../../") is Character:
+		character = get_node("../../")
 
 func _process(delta: float) -> void:
 	particle_material.emission_box_extents.x = character.velocity.length() * delta
