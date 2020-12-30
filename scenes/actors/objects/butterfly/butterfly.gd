1b541dc288c6356abb2a5fe6a723b9ac57ad794d
SuperMakerPlayer <51249300+SuperMakerPlayer@users.noreply.github.com>
2020-12-29 23:23:57 -0800

    add butterflies

---
+++ scenes/actors/objects/butterfly/butterfly.gd
@@ -0,0 +1,24 @@
+extends GameObject
+
+export var custom_preview_position = Vector2(70, 170)
+onready var sprite = $Sprite
+
+var time := 0.0
+var last_position : Vector2
+
+func _ready():
+	CurrentLevelData.enemies_instanced += 1
+	time = (CurrentLevelData.enemies_instanced * 100) / 3
+	sprite.animation = "butterfly" + str((CurrentLevelData.enemies_instanced % 6) + 1)
+	preview_position = custom_preview_position
+	if is_preview:
+		z_index = 0
+		sprite.z_index = 0
+	
+func _physics_process(delta):
+	if mode != 1:
+		time += delta
+		sprite.position.x = sin(time * 2) * 30
+		sprite.position.y = sin(time * 1) * 30
+		sprite.flip_h = true if sign(sprite.position.x - last_position.x) == -1 else false
+		last_position = sprite.position
