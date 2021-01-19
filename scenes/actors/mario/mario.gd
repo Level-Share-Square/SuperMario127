80caafe4c995d79596de6f48c88319fc1475b229
SuperMakerPlayer <51249300+SuperMakerPlayer@users.noreply.github.com>
2021-01-19 12:50:02 -0800

    fix water music in multiplayer
    now, only player 1 affects it

--- scenes/actors/mario/mario.gd
+++ scenes/actors/mario/mario.gd
@@ -527,12 +527,12 @@ func _physics_process(delta: float) -> void:
 	if is_in_water and max_aerial_velocity == 640:
 		gravity_scale = 0.5
 		max_aerial_velocity = 320
-		if music.has_water and !music.play_water:
+		if player_id == 0 and music.has_water and !music.play_water:
 			music.toggle_underwater_music(true)
 	elif !is_in_water and max_aerial_velocity == 320:
 		gravity_scale = 1
 		max_aerial_velocity = 640
-		if music.play_water:
+		if player_id == 0 and music.play_water:
 			music.toggle_underwater_music(false)
 	
 	# Gravity
