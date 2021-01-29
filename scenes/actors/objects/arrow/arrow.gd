338cb198458e4cb335e7dd4e7f111ae9dc56847e
SuperMakerPlayer <51249300+SuperMakerPlayer@users.noreply.github.com>
2021-01-28 17:43:13 -0800

    recolorable arrows

--- scenes/actors/objects/arrow/arrow.gd
+++ scenes/actors/objects/arrow/arrow.gd
@@ -1,13 +1,17 @@
 extends GameObject
 
 var show_behind_player = true
+var color = Color(1, 0, 0)
+
+onready var recolorable = $Recolorable
 
 func _set_properties():
-	savable_properties = ["show_behind_player"]
-	editable_properties = ["show_behind_player"]
+	savable_properties = ["show_behind_player", "color"]
+	editable_properties = ["show_behind_player", "color"]
 
 func _set_property_values(): 
 	set_property("show_behind_player", show_behind_player, true)
+	set_property("color", color, true)
 
 func _ready():
 	preview_position = Vector2(70, 85)
@@ -18,3 +22,6 @@ func _ready():
 		z_index = -2
 	else:
 		z_index = 2
+
+func _process(delta):
+	recolorable.modulate = color
