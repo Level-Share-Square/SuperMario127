7531b3ede065840e84dbe8b835a4a82f0269edf7
SuperMakerPlayer <51249300+SuperMakerPlayer@users.noreply.github.com>
2021-02-27 18:56:50 +0100

    fix resetting level crashing game

--- scenes/editor/ResetLevelButton.gd
+++ scenes/editor/ResetLevelButton.gd
@@ -1,6 +1,7 @@
 extends Button
 
 func _on_ResetLevelButton_pressed():
+	CurrentLevelData.area = 0
 	get_node("/root/CurrentLevelData")._ready()
 	# warning-ignore: return_value_discarded
 	get_tree().reload_current_scene() 
