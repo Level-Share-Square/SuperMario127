ad5c384b5ee42e57644b96caf5201a2ae48a5157
FluentCoding <zSuckyLP@gmail.com>
2021-04-09 15:41:44 +0200

    made bob ombs a little bit faster performance-wise, will come back to that later

--- scenes/actors/objects/bob_omb/bob_omb.gd
+++ scenes/actors/objects/bob_omb/bob_omb.gd
@@ -207,9 +207,7 @@ func _physics_process(delta):
 					position.y -= 4
 				
 		sprite.flip_h = true if facing_direction == 1 else false
-		velocity.y += gravity * gravity_scale
-		velocity.y += gravity * gravity_scale
-		velocity = kinematic_body.move_and_slide_with_snap(velocity, snap, Vector2.UP.normalized(), true, 4, deg2rad(46))
+		velocity = kinematic_body.move_and_slide_with_snap(Vector2(velocity.x, velocity.y + gravity * gravity_scale), snap, Vector2.UP, true, 4, deg2rad(46))
 		if character == null:
 			if walk_wait > 0:
 				sprite.animation = "default"
