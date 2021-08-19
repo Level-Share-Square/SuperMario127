bdd9037066a0cd3b82a81d45908c5efeccfaaddc
JoshuaEagles <joshuaeagles1@gmail.com>
2021-08-18 23:16:40 -0400

    Add back button to main menu

--- scenes/menu/main_menu/main_menu.gd
+++ scenes/menu/main_menu/main_menu.gd
@@ -2,10 +2,9 @@ extends Screen
 
 onready var button_campaign : Button = $Panel/VBoxContainer/ButtonCampaign
 onready var button_levels : Button = $Panel/VBoxContainer/ButtonLevels
-onready var button_editor : Button = $Panel/VBoxContainer/ButtonEditor
 onready var button_templates : Button = $Panel/VBoxContainer/ButtonTemplates
 onready var button_options : Button = $Panel/VBoxContainer/ButtonOptions
-onready var button_quit : Button = $Panel/VBoxContainer/ButtonQuit
+onready var button_quit : Button = $Panel/ButtonQuit
 
 onready var timer = $CooldownTimer
 
@@ -13,7 +12,6 @@ const EDITOR_SCENE : PackedScene = preload("res://scenes/editor/editor.tscn")
 
 func _ready() -> void:
 	var _connect = button_levels.connect("pressed", self, "on_button_levels_pressed")
-	_connect = button_editor.connect("pressed", self, "on_button_editor_pressed")
 	_connect = button_templates.connect("pressed", self, "on_button_templates_pressed")
 	_connect = button_options.connect("pressed", self, "on_button_options_pressed")
 	_connect = button_quit.connect("pressed", self, "on_button_quit_pressed")
@@ -40,11 +38,6 @@ func on_button_levels_pressed() -> void:
 		Singleton.SavedLevels.selected_level = Singleton.SavedLevels.NO_LEVEL
 	timer.start()
 	emit_signal("screen_change", "main_menu_screen", "levels_screen")
-
-func on_button_editor_pressed() -> void:
-	Singleton.SavedLevels.selected_level = Singleton.SavedLevels.NO_LEVEL
-	Singleton.CurrentLevelData.reset() # Reset level
-	var _change_scene = get_tree().change_scene_to(EDITOR_SCENE)
 
 func on_button_templates_pressed() -> void:
 	if timer.time_left > 0:
