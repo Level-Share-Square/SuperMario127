867eb991e0c31a1ef7d4ddf4fff80caca468f0b8
CardboardCarl <31835840+CardboardCarl@users.noreply.github.com>
2021-08-23 20:29:33 -0400

    Set up new variables for local pipe teleportation

---
+++ scenes/actors/objects/teleport_objects/pipe/pipe.gd
@@ -0,0 +1,122 @@
+extends TeleportObject
+
+onready var pipe_enter_logic = $PipeEnterLogic
+onready var sprite = $Sprite
+onready var sprite2 = $Sprite/Sprite2
+
+export var normal_texture : Texture
+export var recolorable_texture : Texture 
+
+var pipe_mode = TELEPORT_MODE.LOCAL
+var area_id := 0
+var pipe_tag := "default_pipe"
+var pipe_destination_tags : PoolStringArray = []
+var color := Color(0, 1, 0)
+
+
+func _set_properties():
+	savable_properties = ["area_id", "pipe_tag", "color", "pipe_destination_tags", "pipe_mode"]
+	editable_properties = ["area_id", "pipe_tag", "color", "pipe_destination_tags", "pipe_mode"]
+	
+func _set_property_values():
+	set_property("area_id", area_id, true)
+	set_property("pipe_tag", pipe_tag, true)
+	set_property("color", color, true)
+	set_property("pipe_mode", pipe_mode, true)
+	set_property("pipe_destination_tags", pipe_destination_tags, true)
+
+func _ready():
+	if rotation != 0 and enabled: #TODO: Vertical & Lateral pipes
+		enabled = false
+	match pipe_mode:
+		PIPE_MODE.REMOTE:
+			pipe_enter_logic.connect("pipe_animation_finished", self, "change_areas")
+		PIPE_MODE.LOCAL:
+			pass
+	if rotation == 0:
+		Singleton.CurrentLevelData.level_data.vars.pipes.append([pipe_tag.to_lower(), self])
+
+func _process(delta): #WHY IS THIS IN _PROCESS
+	if rotation != 0 and enabled:
+		enabled = false
+	if color == Color(0, 1, 0):
+		sprite.texture = normal_texture
+		sprite2.visible = false
+		sprite.self_modulate = Color(1, 1, 1)
+	else:
+		sprite.texture = recolorable_texture
+		sprite2.visible = true
+		sprite.self_modulate = color
+		var bright_color = color
+		bright_color.s /= 1.5
+		bright_color.v *= 1.15
+		sprite2.self_modulate = bright_color
+
+func change_areas(entering_character : Character, entering):
+	var character = get_tree().get_current_scene().get_node(get_tree().get_current_scene().character)
+	if area_id >= Singleton.CurrentLevelData.level_data.areas.size():
+		area_id = Singleton.CurrentLevelData.area
+	if entering:
+		Singleton.CurrentLevelData.level_data.vars.liquid_positions[Singleton.CurrentLevelData.area] = []
+		for liquid in Singleton.CurrentLevelData.level_data.vars.liquids:
+			Singleton.CurrentLevelData.level_data.vars.liquid_positions[Singleton.CurrentLevelData.area].append(liquid[1].save_pos)
+		
+		var powerup_array = [null, null, null]
+		if is_instance_valid(character.powerup):
+			powerup_array[0] = character.powerup.name
+			powerup_array[1] = character.powerup.time_left
+			powerup_array[2] = character.powerup.play_temp_music
+		
+		var nozzle_name = null
+		if character.nozzle != null:
+			nozzle_name = character.nozzle.name
+		
+		Singleton.CurrentLevelData.level_data.vars.transition_character_data = [
+			character.health,
+			character.health_shards,
+			nozzle_name,
+			character.fuel,
+			powerup_array,
+			get_tree().get_current_scene().switch_timer
+		]
+		
+		if is_instance_valid(get_tree().get_current_scene().character2):
+			var character2 = get_tree().get_current_scene().get_node(get_tree().get_current_scene().character2)
+			var nozzle_name_2 = null
+			if character2.nozzle != null:
+				nozzle_name_2 = character2.nozzle.name
+			
+			var powerup_array2 = [null, null, null]
+			if is_instance_valid(character2.powerup):
+				powerup_array2[0] = character2.powerup.name
+				powerup_array2[1] = character2.powerup.time_left
+				powerup_array2[2] = character2.powerup.play_temp_music
+			
+			Singleton.CurrentLevelData.level_data.vars.transition_character_data_2 = [
+				character2.health,
+				character2.health_shards,
+				nozzle_name_2,
+				character2.fuel,
+				powerup_array2,
+				get_tree().get_current_scene().switch_timer
+			]
+		else:
+			Singleton.CurrentLevelData.level_data.vars.transition_character_data_2 = []
+
+		Singleton.CurrentLevelData.level_data.vars.transition_data = [
+			"pipe", 
+			pipe_tag
+		]
+		entering_character.switch_areas(area_id, 0.5)
+	else:
+		entering_character.invulnerable = false
+		entering_character.controllable = true
+		entering_character.movable = true
+		
+		pipe_enter_logic.is_idle = true
+
+func start_exit_anim(character):
+	pipe_enter_logic.start_pipe_exit_animation(character)
+
+func get_bottom_distance():
+	return pipe_enter_logic.PIPE_BOTTOM_DISTANCE - 30
--- scenes/actors/objects/teleport_objects/teleport_object.gd
+++ scenes/actors/objects/teleport_objects/teleport_object.gd
@@ -1,47 +1,25 @@
 extends GameObject
 
-onready var pipe_enter_logic = $PipeEnterLogic
-onready var sprite = $Sprite
-onready var sprite2 = $Sprite/Sprite2
+class_name TeleportObject
 
-export var normal_texture : Texture
-export var recolorable_texture : Texture 
+# ============================================
+# | While I'm doing pipes, I might as well   |
+# | revampthe teleportation system so we can |
+# | consolidate some of this mess.           |
+# ============================================
 
+enum TELEPORT_MODE{LOCAL, REMOTE} #LOCAL = TP within the same level, REMOTE = TP to different level
+
+var teleportation_mode = TELEPORT_MODE.LOCAL
 var area_id := 0
-var pipe_tag := "default"
-var color := Color(0, 1, 0)
-
-func _set_properties():
-	savable_properties = ["area_id", "pipe_tag", "color"]
-	editable_properties = ["area_id", "pipe_tag", "color"]
-	
-func _set_property_values():
-	set_property("area_id", area_id, true)
-	set_property("pipe_tag", pipe_tag, true)
-	set_property("color", color, true)
+var destination_tags : PoolStringArray = []
 
 func _ready():
-	if rotation != 0 and enabled:
-		enabled = false
-	pipe_enter_logic.connect("pipe_animation_finished", self, "change_areas")
-	if rotation == 0:
-		Singleton.CurrentLevelData.level_data.vars.pipes.append([pipe_tag.to_lower(), self])
-
-func _process(delta):
-	if rotation != 0 and enabled:
-		enabled = false
-	if color == Color(0, 1, 0):
-		sprite.texture = normal_texture
-		sprite2.visible = false
-		sprite.self_modulate = Color(1, 1, 1)
-	else:
-		sprite.texture = recolorable_texture
-		sprite2.visible = true
-		sprite.self_modulate = color
-		var bright_color = color
-		bright_color.s /= 1.5
-		bright_color.v *= 1.15
-		sprite2.self_modulate = bright_color
+	match teleportation_mode:
+		TELEPORT_MODE.REMOTE:
+			connect_remote_members()
+		TELEPORT_MODE.LOCAL:
+			connect_local_members()
 
 func change_areas(entering_character : Character, entering):
 	var character = get_tree().get_current_scene().get_node(get_tree().get_current_scene().character)
@@ -106,8 +84,8 @@ func change_areas(entering_character : Character, entering):
 		
 		pipe_enter_logic.is_idle = true
 
-func start_exit_anim(character):
-	pipe_enter_logic.start_pipe_exit_animation(character)
+func connect_local_members():
+	pass
 
-func get_bottom_distance():
-	return pipe_enter_logic.PIPE_BOTTOM_DISTANCE - 30
+func connect_remote_members():
+	pass
