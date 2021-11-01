9ea90ce97f6f8b8e1590e824006a79a411142c45
CardboardCarl <31835840+CardboardCarl@users.noreply.github.com>
2021-11-01 13:00:18 -0400

    fixed door import bug

--- scenes/actors/objects/door/door.gd
+++ scenes/actors/objects/door/door.gd
@@ -28,11 +28,9 @@ func _init():
 func _ready() -> void:
 	.ready() #calls parent class "TeleportObject"
 	if mode == 1:
-		if teleport_to_tag != null || teleport_to_tag != "none":
-			destination_tag = teleport_to_tag
-		if door_tag != null || door_tag != "none":
-			destination_tag = door_tag
-			door_tag = "none"
+		if destination_tag == null || destination_tag != "default_teleporter":
+			if teleport_to_tag != null || teleport_to_tag != "none":
+				destination_tag = teleport_to_tag
 		tag = "none"
 		teleport_to_tag = "none"
 		_set_property_values()
@@ -44,8 +42,10 @@ func _ready() -> void:
 		scale.x = abs(scale.x)
 		sprite.flip_h = true
 	var append_tag = destination_tag.to_lower()
-	if tag != "none":
-		append_tag = tag.to_lower()
+	if teleport_to_tag != "none" || teleport_to_tag != null:
+		append_tag = teleport_to_tag.to_lower()
+	if door_tag != "none" || door_tag != null:
+		append_tag = door_tag.to_lower()
 	Singleton.CurrentLevelData.level_data.vars.teleporters.append([append_tag, self])
 
 
