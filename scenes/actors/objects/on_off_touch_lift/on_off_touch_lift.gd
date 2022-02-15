04d841d3607cdfaf438f23342f47a684bb12d4a3
No U <bene.danika@gmail.com>
2022-02-15 23:30:52 +0100

    Remove unused draw_circle_custom functions in touch lift

--- scenes/actors/objects/on_off_touch_lift/on_off_touch_lift.gd
+++ scenes/actors/objects/on_off_touch_lift/on_off_touch_lift.gd
@@ -153,24 +153,6 @@ func set_sprite_parts(sprite):
 	sprite.rect_position.x = -(left_width + (part_width * parts) + right_width) / 2
 	sprite.rect_size.x = left_width + right_width + part_width * parts
 
-func draw_circle_custom(position, radius, color, maxerror = 0.25):
-	if radius <= 0.0:
-		return
-
-	var maxpoints = 1024 # I think this is renderer limit
-
-	var numpoints = ceil(PI / acos(1.0 - maxerror / radius))
-	numpoints = clamp(numpoints, 3, maxpoints)
-
-	var points = PoolVector2Array([])
-
-	for i in numpoints:
-		var phi = i * PI * 2.0 / numpoints
-		var v = position + Vector2(sin(phi), cos(phi))
-		points.push_back(v * radius)
-
-	draw_colored_polygon(points, color)
-
 func _draw():
 	if mode == 1:
 		draw_polyline(path.curve.get_baked_points(), line_color, 2.0)
