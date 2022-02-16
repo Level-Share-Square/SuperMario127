07450d048e8edadf60806758723b4b364a850908
CardboardCarl <31835840+CardboardCarl@users.noreply.github.com>
2022-02-16 18:30:07 -0500

    fixed block hit sound playing when on side of block

--- scenes/actors/objects/block/block.gd
+++ scenes/actors/objects/block/block.gd
@@ -25,17 +25,14 @@ func _on_hit():
 func _on_hit_body_entered(body):
 	var hit_direction = Vector2(global_position - body.global_position).normalized()
 	hit_direction = Vector2(clamp(hit_direction.x, -1.0, 1.0), clamp(hit_direction.y, -1.0, 1.0))
-	print(Vector2(stepify(hit_direction.x, 1), stepify(hit_direction.y, 1)), body.name)
 	if body.name.begins_with("Character"):
-		if hit_direction.y <= 0 and (hit_direction.x <= 0.708 and hit_direction.x >= -0.708): #0.708 is the approximate value of a diagonal normal
+		if hit_direction.y <= 0 and (hit_direction.x <= 0.650 and hit_direction.x >= -0.650): #0.650 is a little less than the approximate value of a diagonal normal
 			block_hit(Vector2.UP)
-		elif body.state != null && body.state.name == "GroundPoundEndState" and hit_direction.y >= 0 and (hit_direction.x <= 0.708 and hit_direction.x >= -0.708):
+		elif body.state != null && body.state.name == "GroundPoundEndState" and hit_direction.y >= 0 and (hit_direction.x <= 0.650 and hit_direction.x >= -0.650):
 			block_hit(Vector2.DOWN)
 
 	elif body.name.begins_with("Shell"):
-		print(global_position - body.global_position)
-		print(Vector2(global_position - body.global_position).normalized())
-		if (hit_direction.y <= 0.708 and hit_direction.y >= -0.708): #0.708 is the approximate value of a diagonal normal
+		if (hit_direction.y <= 0.650 and hit_direction.y >= -0.650): #0.650 is a little less than the approximate value of a diagonal normal
 			block_hit(Vector2(stepify(hit_direction.x, 1), 0))
 			
 func _start_hit_anim(direction):
