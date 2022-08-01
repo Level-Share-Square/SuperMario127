485135e43d0a76a878b7e257aaeb25aec1d4b525
dignityloss <60553204+dignityloss@users.noreply.github.com>
2022-08-01 21:26:47 +0200

    Revert "Revert "ghost now saves at 0.5kb/s""
    This reverts commit 39800f17fcf986f6c201e885b2b88597b09fcfbd.

--- scenes/actors/mario/GhostMario.gd
+++ scenes/actors/mario/GhostMario.gd
@@ -9,6 +9,55 @@ var ghost_rotation
 var frame_counter = -1
 var sfc = 0
 var play_ghost = false
+const ANIM_IDS : Array = [
+	"x",
+	"armsOut",
+	"bonkedLeft",
+	"bonkedRight",
+	"death", 
+	"deathFall", 
+	"diveLeft", 
+	"diveRight",
+	"doubleFallLeft",
+	"doubleFallRight",
+	"doubleJumpLeft", 
+	"doubleJumpRight", 
+	"enterDoorLeft", 
+	"enterDoorRight", 
+	"exitDoorLeft", 
+	"exitDoorRight", 
+	"fallLeft", 
+	"fallRight", 
+	"flyLeft", 
+	"flyRight", 
+	"groundPoundEndLeft", 
+	"groundPoundEndRight", 
+	"groundPoundLeft", 
+	"groundPoundRight",
+	"idleLeft",
+	"idleRight",
+	"jumpLeft",
+	"jumpRight",
+	"lavaBoost",
+	"movingIn", 
+	"movingLeft",
+	"movingOut",
+	"movingRight", 
+	"pipeExitLeft", 
+	"pipeExitRight",
+	"pipeLeft",
+	"pipeRight",
+	"shineDance", 
+	"shineFall",
+	"spinning", 
+	"starRunLeft", 
+	"starRunRight", 
+	"swimming",
+	"tripleJumpLeft",
+	"tripleJumpRight",
+	"wallSlideLeft",
+	"wallSlideRight",
+]
 
 func _ready():
 	var level_info = Singleton.SavedLevels.get_current_levels()[Singleton.SavedLevels.selected_level]
@@ -26,6 +75,6 @@ func _physics_process(delta):
 			file.close()
 			if frame_counter < ghost_anim.size() - 2:
 				frame_counter += 1
-			animation = ghost_anim[frame_counter]
+			animation = ANIM_IDS[ghost_anim[frame_counter]]
 			rotation_degrees = ghost_rotation[frame_counter]
 			position = ghost_pos[frame_counter]
