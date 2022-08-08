491b07247dc00cdfd03574f817757c8b378d2507
Met3o <69457252+Met3o@users.noreply.github.com>
2022-08-08 21:47:22 +0100

    All available user variables to appear in if statement when first initiated, by default

--- scenes/editor/scripting/ifcon.gd
+++ scenes/editor/scripting/ifcon.gd
@@ -10,18 +10,23 @@ var can_drag = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	get_close_button().visible = false
-	visible = true
-	operator.add_item("=")
-	operator.add_item(">=")
-	operator.add_item("<=")
-	operator.add_item("!=")
-	variables.add_item("variable 1")
-	variables.add_item("variable 2")
-	connect("mouse_entered", self, "_on_mouse_entered")
-	connect("mouse_exited", self, "_on_mouse_exited")
-	variables.connect("item_selected", self, "_on_item_selected")
+ get_close_button().visible = false
+ visible = true
+ operator.add_item("=")
+ operator.add_item(">=")
+ operator.add_item("<=")
+ operator.add_item("!=")
+ variables.add_item("variable 1")
+ variables.add_item("variable 2")
 
+ #check if any extra variables are created and add them to the options. "i" is not numerical; it is dictionary words (var names).
+ for i in scripting.variables.keys():
+	 #add item (i is the name)
+	 variables.add_item(str(i))
+
+ connect("mouse_entered", self, "_on_mouse_entered")
+ connect("mouse_exited", self, "_on_mouse_exited")
+ variables.connect("item_selected", self, "_on_item_selected")
 
 func _process(delta):
 	if scripting.instances.size() > 0:
@@ -29,12 +34,12 @@ func _process(delta):
 			if !appended_variables.has(get_newest_var()): #Checks if the newest variable is already added to the list
 				variables.add_item(get_newest_var()) #Adds the newest variable to the list (the name comes from the list of keys in the variables dictionary)
 				appended_variables.append(get_newest_var()) #Appends the newest variable to the appended_variables array so that the game can check if it's already there or not
-			
+
 func get_newest_var() -> String:
 	var variable
 	variable = scripting.variable_list[scripting.variable_num] #Gets the name of the variable from the variable_list array (holds all the keys in the variables dictionary)
 	return variable #Returns the value of variable to be used in the code in _process()
-	
+
 func _input(event: InputEvent) -> void:
 	if can_drag == true:
 		if event is InputEventMouseMotion:
@@ -50,14 +55,14 @@ func _on_mouse_exited():
 
 
 
-	
+
 func _on_item_selected(index):
 	match operator.selected:
 		0:
 			var selected = variables.get_item_text(index)
 			if value.text == scripting.variables.get(str(selected)):
 				pass
-		1:	
+		1:
 			var selected = variables.get_item_text(index)
 			if value.text >= scripting.variables.get(str(selected)):
 				pass
@@ -69,4 +74,3 @@ func _on_item_selected(index):
 			var selected = variables.get_item_text(index)
 			if value.text != scripting.variables.get(str(selected)):
 				pass
-
