6146f4acacddf33513cc657bdaae0f75d3ceded8
ZeroPlayerRodent <yuRodentoh@protonmail.com>
2023-01-16 19:26:12 -0400

    Added option to make water bottles not respawn

--- scenes/actors/objects/small_bottle/small_bottle.gd
+++ scenes/actors/objects/small_bottle/small_bottle.gd
@@ -4,14 +4,25 @@ onready var sprite = $Sprite
 onready var area = $Area2D
 onready var sound = $AudioStreamPlayer
 
+var respawns := true
 var collected = false
 var respawn_timer = 0.0
-		
+
+func _set_properties() -> void:
+	savable_properties = ["respawns"]
+	editable_properties = ["respawns"]
+	
+func _set_property_values() -> void:
+	set_property("respawns", respawns, true)
+
 func collect(body):
 	if enabled and !collected and body.name.begins_with("Character") and !body.dead:
 		sound.play()
 		sprite.visible = false
-		respawn_timer = 40.0
+		if respawns:
+			respawn_timer = 40.0
+		else:
+			respawn_timer = 0 #If the timer's value is 0, the bottle will not respawn
 		body.fuel += 15
 		if body.fuel > 100:
 			body.fuel = 100
