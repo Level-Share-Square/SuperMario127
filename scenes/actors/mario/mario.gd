1c99b281f30805c6680ea432868c14fae840d6f4
ZeroPlayerRodent <yuRodentoh@protonmail.com>
2023-01-21 21:13:16 -0400

    Fixed Mario getting stuck on falling logs

--- scenes/actors/mario/mario.gd
+++ scenes/actors/mario/mario.gd
@@ -409,7 +409,7 @@ func is_grounded() -> bool:
 		return true
 	
 	var raycast_node := ground_check
-	raycast_node.cast_to = Vector2(0, 26)
+	raycast_node.cast_to = Vector2(0, 30) #26
 	if !ground_collision_dive.disabled:
 		raycast_node = ground_check_dive
 		raycast_node.cast_to = Vector2(0, 7.5)
