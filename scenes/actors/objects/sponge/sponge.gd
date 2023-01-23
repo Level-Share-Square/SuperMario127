7aff0012ec6e180623c63a7faeafa0b97eb1784b
Jordi <37673423+jordi-exe@users.noreply.github.com>
2023-01-23 16:36:27 -0500

    Manual Sponge
    - Created the sponge
    - Only hardcoded atm (hence manual)

--- scenes/actors/objects/sponge/sponge.gd
+++ scenes/actors/objects/sponge/sponge.gd
@@ -1,12 +1,40 @@
 extends GameObject
 
+var _timer = null
 
+var water_drain_speed = 5
+var water_in_sponge = 0
+var max_water = 50
+var player_in_area = false
+var body
 
+#applies the timer
+func _ready():
+	_timer = Timer.new()
+	add_child(_timer)
+	_timer.connect("timeout", self, "_on_Timer_timeout")
+	_timer.set_wait_time(1.0)
+	_timer.set_one_shot(false) # Make sure it loops
 
-func _on_Area2D_area_entered(area):
+#the opposite of body_entered and stops the timer
+func _on_Area2D_body_exited(body1):
+	if player_in_area == true and body1.name.begins_with("Character") and !body1.dead:
+		player_in_area = false
+		_timer.stop()
+		print("Body exited")
 
-	if area == area.name.begins_with("Character"):
-		#if water_in_sponge < max_water:
-		  #player.water -= water_drain_speed
-		  #water_in_sponge += water_drain_speed
-		print(area.name.begins_with("Character"))
+#checks if the player has entered the area2D and starts the timer
+func _on_Area2D_body_entered(body1):
+	if player_in_area == false and body1.name.begins_with("Character") and !body1.dead:
+		print("Body Entered")
+		player_in_area = true
+		body = body1
+		_timer.start()
+		print("Timer started")
+
+#triggers everytime the timer has ran
+func _on_Timer_timeout():
+	if player_in_area == true and body.name.begins_with("Character") and !body.dead:
+		if water_in_sponge < max_water:
+			body.fuel -= water_drain_speed
+			water_in_sponge += water_drain_speed
