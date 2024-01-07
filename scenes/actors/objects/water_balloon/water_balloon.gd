cae3eb8f578b0d68db3202d9b81ef1506545a9da
cap39 <buzzkill619@gmail.com>
2024-01-06 18:19:17 -0600

    recolorable balloon

--- scenes/actors/objects/water_balloon/water_balloon.gd
+++ scenes/actors/objects/water_balloon/water_balloon.gd
@@ -5,23 +5,28 @@ onready var drop = $Sprite/Sprite2
 onready var area = $Area2D
 onready var sound = $AudioStreamPlayer
 
+export var normal_texture : Texture
+export var recolorable_texture : Texture 
+
 var added_stamina = 100
 var added_water = 50
 var collected = false
 var respawn_timer = 10.0
+var color := Color(0, 0.7, 1)
 
 var timer = 0.0
 
 		
 		
 func _set_properties():
-	savable_properties = ["added_stamina", "added_water", "respawn_timer"]
-	editable_properties = ["added_stamina", "added_water", "respawn_timer"]
+	savable_properties = ["added_stamina", "added_water", "respawn_timer", "color"]
+	editable_properties = ["added_stamina", "added_water", "respawn_timer", "color"]
 	
 func _set_property_values():
 	set_property("added_stamina", added_stamina)
 	set_property("added_water", added_water)
 	set_property("respawn_timer", respawn_timer)
+	set_property("color", color)
 
 func collect(body):
 	if enabled and !collected and body.name.begins_with("Character") and !body.dead:
@@ -37,15 +42,24 @@ func collect(body):
 		if body.stamina > 100:
 			body.stamina = 100
 		collected = true
+		#bounce
 		if !body.big_attack and !body.invincible and body.velocity.y > -325:
+			body.set_state_by_name("BounceState", 0)
 			body.velocity.y = -325
 
 func _ready():
 	$AnimationPlayer.play("bpb")
+	connect("property_changed", self, "_on_property_changed")
 	if is_preview:
 		z_index = 0
 		sprite.z_index = 0
 	var _connect = area.connect("body_entered", self, "collect")
+	if color == Color(0, 0.7, 1):
+		sprite.texture = normal_texture
+		sprite.self_modulate = Color(1, 1, 1)
+	else:
+		sprite.texture = recolorable_texture
+		sprite.self_modulate = color
 	
 func _process(delta):
 	if added_water == 0:
@@ -60,3 +74,12 @@ func _process(delta):
 			sprite.visible = true
 			$Particles2D.emitting = true
 			collected = false
+			
+func _on_property_changed(key, value):
+	if key == "color":
+		if color == Color(0, 0.7, 1):
+			sprite.texture = normal_texture
+			sprite.self_modulate = Color(1, 1, 1)
+		else:
+			sprite.texture = recolorable_texture
+			sprite.self_modulate = value
