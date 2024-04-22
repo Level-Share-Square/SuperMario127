5257eb6872cf9f151940e1f9843ac7cc8e93a85b
cap39 <buzzkill619@gmail.com>
2024-04-22 12:17:38 -0500

    fixed camera stopping objects

--- classes/camera_stopper.gd
+++ classes/camera_stopper.gd
@@ -1,5 +1,5 @@
-extends Area2D
 class_name CameraStopper
+extends Area2D
 
 var top_bound
 var bottom_bound 
@@ -7,17 +7,15 @@ var left_bound
 var right_bound 
 var editor_rect
 
+onready var shape = $CollisionShape2D
+
 func _ready():
-	var size = get_child(0).shape.extents
-	top_bound = global_transform.xform(Vector2(0, -size.y))
-	bottom_bound = global_transform.xform(Vector2(0, size.y))
-	left_bound = global_transform.xform(Vector2(-size.x, 0))
-	right_bound = global_transform.xform(Vector2(size.x, 0))
+	set_size(shape.shape.extents)
 	set_collision_layer_bit(11, true)
 	
 	if get_tree().get_current_scene().mode != 0:
 		editor_rect = ReferenceRect.new()
-		editor_rect.rect_size = size * 2
+		editor_rect.rect_size = get_child(0).shape.extents * 2
 		editor_rect.border_color = Color.aqua
 		editor_rect.border_width = 2.0
 		editor_rect.editor_only = false
@@ -29,3 +27,10 @@ func _process(_delta):
 		editor_rect.rect_size = get_child(0).shape.extents * 2
 		editor_rect.rect_position.x = -editor_rect.rect_size.x / 2
 		editor_rect.rect_position.y = -editor_rect.rect_size.y / 2
+
+		
+func set_size(size):
+	top_bound = global_transform.xform(Vector2(0, -size.y))
+	bottom_bound = global_transform.xform(Vector2(0, size.y))
+	left_bound = global_transform.xform(Vector2(-size.x, 0))
+	right_bound = global_transform.xform(Vector2(size.x, 0))
--- scenes/actors/objects/area_transition/area_transition.gd
+++ scenes/actors/objects/area_transition/area_transition.gd
@@ -57,7 +57,11 @@ func _ready() -> void:
 		camera_stopper.monitorable = stops_camera
 	else:
 		var _connect2 = connect("property_changed", self, "update_property")
+		camera_stopper.visible = stops_camera
 		
+	update_property("vertical", vertical)
+	camera_stopper.set_size(camera_stop_shape.shape.extents)
+	
 func update_property(key, value):
 	match(key):
 		"parts":
@@ -65,63 +69,66 @@ func update_property(key, value):
 		"vertical":
 			if vertical:
 #				sprite.rect_size.x = 32
-#				sprite.rect_position.x = -16
-#				collision_shape.shape.extents.x = 16
-#				camera_stop_shape.shape.extents.x = 52
-				sprite.rect_size.y = 32
-				sprite.rect_position.y = -16
-				collision_shape.shape.extents.y = 16
-				camera_stop_shape.shape.extents.y = 52
-				rotation_degrees = 270
+				sprite.rect_position.x = -16
+				sprite.rect_rotation = 270
+				collision_shape.shape.extents.x = 16
+				camera_stop_shape.shape.extents.x = 52
 			else:
-				sprite.rect_size.y = 32
+#				sprite.rect_size.y = 32
 				sprite.rect_position.y = -16
+				sprite.rect_rotation = 0
 				collision_shape.shape.extents.y = 16
 				camera_stop_shape.shape.extents.y = 52
 			update_parts()
 		"rotation_degrees":
 			rotation_degrees = 0
+		"stops_camera":
+			camera_stopper.visible = value
 			
 func update_parts():
+	sprite.rect_size.x = parts * 32
 	if vertical:
 #		sprite.rect_size.y = parts * 32
-#		sprite.rect_position.y = (-16 * parts)
-#		collision_shape.shape.extents.y = 16 * parts
-#		camera_stop_shape.shape.extents.y = collision_shape.shape.extents.y + 26
-		sprite.rect_size.x = parts * 32
-		sprite.rect_position.x = (-16 * parts)
-		collision_shape.shape.extents.x = 16 * parts
-		camera_stop_shape.shape.extents.x = collision_shape.shape.extents.x + 26
+		sprite.rect_position.y = (16 * parts) - 32
+		collision_shape.shape.extents.y = 16 * parts
+		camera_stop_shape.shape.extents.y = collision_shape.shape.extents.y + 26
 	else:
-		sprite.rect_size.x = parts * 32
+#		sprite.rect_size.x = parts * 32
 		sprite.rect_position.x = (-16 * parts)
 		collision_shape.shape.extents.x = 16 * parts
 		camera_stop_shape.shape.extents.x = collision_shape.shape.extents.x + 26
-		
-		
+	
+
 func body_entered(body):
 	if enabled and body.name.begins_with("Character") and !body.dead and body.controllable and teleport_enabled:
-		print("teleporting")
 		body.toggle_movement(false)
+		body.gravity_scale = 0
 		change_areas(body, true)
+		# change this to work with multiplayer
+		Singleton.CurrentLevelData.level_data.vars.transition_character_data.append(AreaTransitionHelper.new(body.velocity, body.state.name, body.facing_direction, to_local(body.global_position).x, vertical))
 		
 func body_exited(body):
 	if enabled and body.name.begins_with("Character") and !body.dead:
 		teleport_enabled = true
 		
 func start_exit_anim(character):
-	print("exit anim")
-	print(character.global_position)
 	# to prevent teleport loop
 	teleport_enabled = false;
 	character.toggle_movement(true)
+	character.gravity_scale = 1
 #	character.velocity = Singleton.CurrentLevelData.level_data.vars.transition_data[3]
 #	character.show()
 #	character.sprite.animation = "pipeRight"
 #	character.sprite.playing = true
 #	character.sprite.frame = 2
+
+	# this means we came from another area transition
+	if Singleton.CurrentLevelData.level_data.vars.transition_character_data.size() >= 7:
+		var helper = Singleton.CurrentLevelData.level_data.vars.transition_character_data.back()
+		character.camera.global_position = helper.find_camera_position(vertical, character.global_position, character.camera.base_size)
+		character.camera.last_position = character.camera.global_position
+		Singleton.CurrentLevelData.level_data.vars.transition_data = []
 	reset_sprite(character)
-	Singleton.CurrentLevelData.level_data.vars.transition_data = []
 	
 func reset_sprite(character : Character): #This is here in case Mario came from a door to a pipe
 	character.z_index = -1
