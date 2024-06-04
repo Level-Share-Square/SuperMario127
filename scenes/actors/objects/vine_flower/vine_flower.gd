74ed9e451dda6a24763bb10164699e0cb609ce60
Linktober <linkmaster1014@gmail.com>
2024-06-04 00:53:24 -0500

    Vines Object Mountain BG
    Added the start of the vines object. Need to give thorns damage and finish the palletes.
    Finished implementing the Mountain BG with parallax.

---
+++ scenes/actors/objects/vine_flower/vine_flower.gd
@@ -0,0 +1,31 @@
+extends GameObject
+
+export var custom_preview_position = Vector2(70, 170)
+onready var collision_shape = $StaticBody2D/CollisionShape2D
+export(Array, Texture) var palette_textures
+
+var color := Color(1, 0, 0)
+
+func _set_properties():
+	savable_properties = ["color"]
+	editable_properties = ["color"]
+	
+func _set_property_values():
+	set_property("color", color, 1)
+
+func _ready():
+	collision_shape.disabled = !enabled
+	preview_position = custom_preview_position
+	if is_preview:
+		z_index = 0
+		$Sprite.z_index = 0
+		
+	if palette != 0:
+		$Sprite.texture = palette_textures[palette - 1]
+
+func _process(delta):
+	if color == Color(1, 0, 0):
+		$Color.visible = false
+	else:
+		$Color.visible = true
+		$Color.modulate = color
