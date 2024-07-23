031be94159ec42afe1ae673f3db15a172292b807
Tricia <gtcreyon@gmail.com>
2024-07-23 19:28:43 +0100

    Fixed moving platforms real

--- scenes/actors/objects/metal_platform/semi_solid_platform.gd
+++ scenes/actors/objects/metal_platform/semi_solid_platform.gd
@@ -1,6 +1,8 @@
 class_name SemiSolidPlatform
 extends PhysicsBody2D
 
+const THRESHOLD: float = 2.5
+
 var buffer := -5
 var movement := Vector2.ZERO
 
@@ -9,29 +11,27 @@ func can_collide_with(character):
 	var direction = global_transform.y.normalized()
 	
 	var is_grounded = character.is_grounded() if character.has_method("is_grounded") else true
-	# Some math that gives us useful vectors
 	var line_center = global_position + (direction * buffer)
 	var line_direction = Vector2(-direction.y, direction.x)
 	var p1 = line_center + line_direction
 	var p2 = line_center - line_direction
-	var p = character.bottom_pos.global_position #if is_grounded else character.global_position
-	#var velocity = character.velocity if character.get("velocity") != null else Vector2(0, 0) seems to be unused, uncomment if needed
+	var p = character.bottom_pos.global_position
+	var velocity = (character.velocity / 60.0) if character.get("velocity") != null else Vector2.ZERO
 	var diff = p2 - p1
 	var perp = Vector2(-diff.y, diff.x)
 	
-	if !is_grounded:
-		# If in the air, check for the velocity first
-		# If we're trying to pass through it from the other way around,
-		# cancel it
-		var d = (character.velocity / 60.0 - movement).dot(perp)
-		if d < 0:
-			return false
-		
-		# Account for the movement of the platform to prevent clipping.
-		p += movement
-	else:
+	if is_grounded:
 		p -= perp
 	
+	var perp_norm = perp.normalized()
+	var vel_dir = (velocity - movement).dot(perp_norm)
+	var dynamic_threshold = vel_dir / THRESHOLD + 0.01
 	# Is p on the correct side?
-	var d = (p - p1).dot(perp)
-	return sign(d) != 1
+	var side = (p - p1).dot(perp_norm)
+	var correct_side = side < dynamic_threshold
+	
+	# If we're trying to pass through it from the underside, cancel collision
+	if not is_grounded and vel_dir < 0:
+		return false
+
+	return correct_side
