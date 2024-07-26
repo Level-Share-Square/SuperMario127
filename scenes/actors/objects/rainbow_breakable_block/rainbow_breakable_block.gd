cceb921f644e1b1730a96073941342bc53c34798
cap39 <buzzkill619@gmail.com>
2024-07-26 00:29:20 -0500

    rainbow block rewrite

--- scenes/actors/objects/rainbow_breakable_block/rainbow_breakable_block.gd
+++ scenes/actors/objects/rainbow_breakable_block/rainbow_breakable_block.gd
@@ -2,10 +2,10 @@ extends GameObject
 
 onready var sprite = $Sprite
 onready var area = $Area2D
+onready var area_shape = $Area2D/CollisionShape2D
 onready var static_body = $StaticBody2D
 onready var collision_shape = $StaticBody2D/CollisionShape2D
-onready var stomp_area = $StompArea
-onready var spin_area = $SpinArea
+
 onready var broken_sound = $Broken
 onready var break_particle = $BreakParticle
 var broken = false
@@ -22,20 +22,26 @@ func _set_property_values():
 	set_property("coins", coins, true)
 
 func _ready():
-	if !enabled:
+
+
+	if enabled:
+		if mode != 1:
+			print("jdwjk")
+			area.connect("body_entered", self, "_body_entered")
+			area.connect("body_exited", self, "_body_exited")
+		if scale != Vector2.ONE: # Nothing to do on default scale
+			# Set inverse scale on the body so its overall scale is identity.
+			# For whatever reason, division doesn't work on vectors, soo
+			static_body.scale = Vector2(1.0 / scale.x, 1.0 / scale.y)
+			# So it doesn't modify all other boxes
+			collision_shape.shape = collision_shape.shape.duplicate()
+			# Modify the extents by the scale to get the desired collision shape
+			collision_shape.shape.extents = Vector2(collision_shape.shape.extents.x * scale.x,\
+													collision_shape.shape.extents.y * scale.y)
+	else:
 		collision_shape.disabled = true
-		for _area in [area, stomp_area, spin_area]:
-			_area.collision_layer = 0
-			_area.collision_mask = 0
-	elif scale != Vector2.ONE: # Nothing to do on default scale
-		# Set inverse scale on the body so its overall scale is identity.
-		# For whatever reason, division doesn't work on vectors, soo
-		static_body.scale = Vector2(1.0 / scale.x, 1.0 / scale.y)
-		# So it doesn't modify all other boxes
-		collision_shape.shape = collision_shape.shape.duplicate()
-		# Modify the extents by the scale to get the desired collision shape
-		collision_shape.shape.extents = Vector2(collision_shape.shape.extents.x * scale.x,\
-												collision_shape.shape.extents.y * scale.y)
+		area_shape.disabled = true
+		
 	
 	break_particle.hide()
 	sprite.material.set_shader_param("gradient", Singleton.MiscShared.rainbow_gradient_texture)
@@ -52,41 +58,42 @@ func handle_character_exception(character: Character):
 		static_body.remove_collision_exception_with(character)
 
 func _physics_process(delta):
-	if mode != 1 and enabled:
-		time_alive += delta
-		
-		for hit_body in stomp_area.get_overlapping_bodies():
-			if !broken and hit_body.name.begins_with("Character"): if is_rainbow(hit_body):
-				broken = true
-				if not broken_sound.is_playing(): 
-					for i in(coins): create_coin()
-					break_particle.show()
-					break_particle.set_emitting(true)
-					broken_sound.play()
-					yield(get_tree().create_timer(3.0), "timeout")
-					queue_free() # die
-		for hit_body in spin_area.get_overlapping_bodies():
-			if !broken and hit_body.name.begins_with("Character"): if is_rainbow(hit_body):
-				broken = true
-				if not broken_sound.is_playing(): 
-					for i in(coins): create_coin()
-					break_particle.show()
-					break_particle.set_emitting(true)
-					broken_sound.play()
-					yield(get_tree().create_timer(3.0), "timeout")
-					queue_free() # die
-		
-		var scene : Node = get_tree().current_scene
-		if scene.has_node(scene.character):
-			handle_character_exception(scene.get_node(scene.character))
-		if scene.has_node(scene.character2):
-			handle_character_exception(scene.get_node(scene.character2))
-		
-		if broken:
-			sprite.visible = false
-			static_body.set_collision_layer_bit(0, false)
-			static_body.set_collision_mask_bit(1, false)
-			stomp_area.set_collision_layer_bit(0, false)
+	pass
+#		if mode != 1 and enabled:
+#			time_alive += delta
+#
+#			for hit_body in stomp_area.get_overlapping_bodies():
+#				if !broken and hit_body.name.begins_with("Character"): if is_rainbow(hit_body):
+#					broken = true
+#					if not broken_sound.is_playing(): 
+#						for i in(coins): create_coin()
+#						break_particle.show()
+#						break_particle.set_emitting(true)
+#						broken_sound.play()
+#						yield(get_tree().create_timer(3.0), "timeout")
+#						queue_free() # die
+#			for hit_body in spin_area.get_overlapping_bodies():
+#				if !broken and hit_body.name.begins_with("Character"): if is_rainbow(hit_body):
+#					broken = true
+#					if not broken_sound.is_playing(): 
+#						for i in(coins): create_coin()
+#						break_particle.show()
+#						break_particle.set_emitting(true)
+#					broken_sound.play()
+#					yield(get_tree().create_timer(3.0), "timeout")
+#					queue_free() # die
+#
+#		var scene : Node = get_tree().current_scene
+#		if scene.has_node(scene.character):
+#			handle_character_exception(scene.get_node(scene.character))
+#		if scene.has_node(scene.character2):
+#			handle_character_exception(scene.get_node(scene.character2))
+#
+#		if broken:
+#			sprite.visible = false
+#			static_body.set_collision_layer_bit(0, false)
+#			static_body.set_collision_mask_bit(1, false)
+#			stomp_area.set_collision_layer_bit(0, false)
 
 func create_coin(): #creates a coin
 	time_alive += 1
@@ -104,3 +111,27 @@ func create_coin(): #creates a coin
 	var velocity_x = -power if int(time_alive * 10) % 2 == 0 else power
 	object.properties.append(Vector2(velocity_x, -300)) #makes the coin move around and fly in the air when the block breaks
 	get_parent().create_object(object, false) #finishes the object creation
+	
+func break_block():
+	broken = true
+	if not broken_sound.is_playing(): 
+		for i in(coins): create_coin()
+		break_particle.show()
+		break_particle.set_emitting(true)
+	broken_sound.play()
+	sprite.visible = false
+	collision_shape.disabled = true
+	static_body.set_collision_layer_bit(0, false)
+	static_body.set_collision_mask_bit(1, false)
+	area_shape.disabled = true
+	yield(get_tree().create_timer(3.0), "timeout")
+	queue_free() # die
+	
+func _body_entered(body):
+	print("entered")
+	if body.name.begins_with("Character") and is_rainbow(body) and body.global_position.y > area.global_position.y:
+		break_block()
+
+func _body_exited(body):
+	if body.name.begins_with("Character") and is_rainbow(body) and body.global_position.y < area.global_position.y:
+		break_block()
