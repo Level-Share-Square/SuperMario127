bca843d06e0d27bd73b4740555e570250615e671
aloelucidity <106764798+aloelucidity@users.noreply.github.com>
2024-08-06 22:19:56 +0200

    Update FluddAnimSync.gd

--- scenes/actors/mario/FluddAnimSync.gd
+++ scenes/actors/mario/FluddAnimSync.gd
@@ -2,7 +2,7 @@ extends AnimatedSprite
 
 onready var mario_sprite : AnimatedSprite = get_parent()
 
-func _physics_process(_delta):
+func _process(_delta):
 	animation = mario_sprite.animation
 	frame = mario_sprite.frame
 	flip_h = mario_sprite.flip_h
