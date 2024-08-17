0e271f3d7c6e09770abf0be39224af4cf099ccb8
aloelucidity <106764798+aloelucidity@users.noreply.github.com>
2024-08-17 17:23:56 +0200

    make mouse more responsive

--- scenes/menu/ui_focus.gd
+++ scenes/menu/ui_focus.gd
@@ -5,8 +5,14 @@ onready var default_focus: Button = get_node(default_focus_path)
 
 func _input(event):
 	if not get_parent().visible: return
+	
+	if get_focus_owner() != null and event is InputEventMouseMotion and !Input.is_mouse_button_pressed(1): 
+		get_focus_owner().release_focus()
+
+func _unhandled_input(event):
+	if not get_parent().visible: return
 	if not is_instance_valid(default_focus): return
-		
+	
 	if get_focus_owner() == null:
 		if (event.is_action_pressed("ui_left")
 		or event.is_action_pressed("ui_right")
@@ -15,5 +21,3 @@ func _input(event):
 			# we don't want the cursor to move immediately after grabbing focus
 			yield(get_tree(), "idle_frame")
 			default_focus.grab_focus()
-	else:
-		if event is InputEventMouseMotion: get_focus_owner().release_focus()
