1caa3b45fcb42db71ac4c6df83e4885d40ed593a
aloelucidity <aloelucidi@gmail.com>
2024-09-11 18:33:57 +0200

    deleting a level now also deletes related music and thumbnail files

--- scenes/menu/levels_list/list_scripts/saver.gd
+++ scenes/menu/levels_list/list_scripts/saver.gd
@@ -21,3 +21,19 @@ func delete_level(level_id: String, working_folder: String):
 	
 	var file_path: String = saved_levels_util.get_level_file_path(level_id, working_folder)
 	saved_levels_util.delete_file(file_path)
+	
+	# we should be cleaning up any related files as well :)
+	var thumbnail_path: String = saved_levels_util.get_level_thumbnail_path(level_id, working_folder)
+	if saved_levels_util.file_exists(thumbnail_path):
+		saved_levels_util.delete_file(thumbnail_path)
+	
+	var music_folder: String = saved_levels_util.get_level_music_folder(working_folder)
+	var directory := Directory.new()
+	if directory.open(music_folder) == OK:
+		directory.list_dir_begin(true)
+		
+		var file: String = directory.get_next()
+		while file != "":
+			if file.begins_with(level_id):
+				saved_levels_util.delete_file(music_folder + "/" + file)
+			file = directory.get_next()
