f65c0121faa8ca67acdd65fbdec305c51dfdd135
aloelucidity <aloelucidi@gmail.com>
2024-09-11 15:54:51 +0200

    importing levels in html5 now works via txt file upload

--- scenes/menu/levels_list/list_scripts/element_add_buttons.gd
+++ scenes/menu/levels_list/list_scripts/element_add_buttons.gd
@@ -7,7 +7,13 @@ export var list_handler_path: NodePath
 onready var list_handler: Node = get_node(list_handler_path)
 
 func import_level_code():
-	list_handler.insert_level(level_code_edit.text)
+	# if the text box is empty it'll just import
+	# directly from your clipboard
+	var level_code: String = level_code_edit.text
+	if level_code == "" and not OS.has_feature("JavaScript"):
+		level_code = OS.clipboard
+	
+	list_handler.insert_level(level_code)
 	
 	level_code_edit.text = ""
 	level_code_edit.clear_undo_history()
