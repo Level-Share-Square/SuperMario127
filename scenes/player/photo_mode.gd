3e2b1be996a5011fe75d64594b5a762e1bb38ebc
aloelucidity <aloelucidi@gmail.com>
2024-09-23 23:13:28 +0200

    "photo mode" now pauses the game again

--- scenes/player/photo_mode.gd
+++ scenes/player/photo_mode.gd
@@ -19,10 +19,13 @@ func _input(event):
 	if !Singleton.SceneTransitions.transitioning and (!Singleton.ModeSwitcher.get_node("ModeSwitcherButton").switching_disabled or Singleton.ModeSwitcher.get_node("ModeSwitcherButton").invisible) and event.is_action_pressed("toggle_ui") and !(get_tree().paused and !Singleton.PhotoMode.enabled):
 		if !(character_node.dead or (Singleton.PlayerSettings.number_of_players != 1 and character2_node.dead)):
 			Singleton.PhotoMode.enabled = !Singleton.PhotoMode.enabled
-			update_photo_mode(false)
+			update_photo_mode(true)
 		
 	
 func update_photo_mode(do_pause = true):
 	var is_photo_mode = Singleton.PhotoMode.enabled
 	offset.y = 1000000 if is_photo_mode else 0 # hax
-	#get_tree().paused = is_photo_mode and do_pause
+	get_tree().paused = is_photo_mode and do_pause
+	
+	if do_pause:
+		Singleton.CurrentLevelData.can_pause = not is_photo_mode
