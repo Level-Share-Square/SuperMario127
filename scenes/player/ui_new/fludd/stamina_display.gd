87b117c333a2cafc01fabd90eb9865ab2fdd7b36
aloelucidity <aloelucidi@gmail.com>
2024-09-24 22:46:16 +0200

    oops

--- scenes/player/ui_new/fludd/stamina_display.gd
+++ scenes/player/ui_new/fludd/stamina_display.gd
@@ -28,3 +28,13 @@ func _physics_process(delta):
 	var player_pos: Vector2 = character.get_global_transform_with_canvas().get_origin()
 	rect_position = player_pos + display_offset
 	modulate.a = lerp(modulate.a, alpha if is_visible else 0, delta * fade_speed)
+
+
+## for the ui hiding stuff
+func _ready():
+	Singleton.PhotoMode.connect("photo_mode_changed", self, "toggle_photo_mode")
+	toggle_photo_mode()
+
+
+func toggle_photo_mode():
+	visible = not Singleton.PhotoMode.enabled
