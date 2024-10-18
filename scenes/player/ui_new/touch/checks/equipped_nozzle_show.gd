1ba1da0af47848c2b6cc5887af78cedeac995b50
aloelucidity <aloelucidi@gmail.com>
2024-10-18 15:19:25 +0200

    fix constant sawblade erroring

--- scenes/player/ui_new/touch/checks/equipped_nozzle_show.gd
+++ scenes/player/ui_new/touch/checks/equipped_nozzle_show.gd
@@ -5,7 +5,7 @@ extends TouchCheck
 func _input(_event):
 	if is_instance_valid(character): return
 	._check()
-	if not is_connected("nozzle_changed", self, "nozzle_changed"):
+	if not character.is_connected("nozzle_changed", self, "nozzle_changed"):
 		character.connect("nozzle_changed", self, "nozzle_changed")
 
 
