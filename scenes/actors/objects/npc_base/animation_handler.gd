8081d1e743468c9b62cbbf6cb5e129f8fd0466a3
aloelucidity <aloelucidi@gmail.com>
2024-11-09 12:57:23 +0100

    peach hair physics

--- scenes/actors/objects/npc_base/animation_handler.gd
+++ scenes/actors/objects/npc_base/animation_handler.gd
@@ -5,6 +5,7 @@ onready var visibility_notifier = $"%VisibilityNotifier2D"
 
 export var head_positions: Dictionary
 export var expression_offsets: Dictionary
+export var head_transforms: bool = true
 
 onready var head = $Head
 onready var body = $Body
@@ -23,18 +24,19 @@ func _process(delta):
 	head.offset = Vector2.ZERO
 	head.rotation_degrees = 0
 	
-	if head_anim == "confused":
-		head.rotation_degrees = 10
-		head.offset = Vector2(1, 0)
-	
-	if head_anim == "raging":
-		head.modulate = lerp(head.modulate, Color.red, delta)
-		head.offset = Vector2(
-			rand_range(-1.0, 1.0),
-			rand_range(0, 2.0)
-		)
-	else:
-		head.modulate = lerp(head.modulate, Color.white, delta * 4)
+	if head_transforms:
+		if head_anim == "confused":
+			head.rotation_degrees = 10
+			head.offset = Vector2(1, 0)
+		
+		if head_anim == "raging":
+			head.modulate = lerp(head.modulate, Color.red, delta)
+			head.offset = Vector2(
+				rand_range(-1.0, 1.0),
+				rand_range(0, 2.0)
+			)
+		else:
+			head.modulate = lerp(head.modulate, Color.white, delta * 4)
 	
 	if head_anim in expression_offsets:
 		head.offset = expression_offsets[head_anim]
