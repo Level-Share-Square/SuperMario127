7a482a1c7fbb5b37aedd0ffe6a213d8b538f72c8
aloelucidity <aloelucidi@gmail.com>
2024-11-11 21:04:50 +0100

    fully fix star bit counter

--- scenes/player/ui_new/collectibles/purple_starbits.gd
+++ scenes/player/ui_new/collectibles/purple_starbits.gd
@@ -20,21 +20,22 @@ var variables: LevelVars = Singleton.CurrentLevelData.level_data.vars
 
 
 func _ready():
+	hide()
+	
 	# sigh, have to wait for the player scene to finish up their work
 	yield(get_tree(), "physics_frame")
 	yield(get_tree(), "physics_frame")
 	
 	max_purples = variables.max_purple_starbits
+	if max_purples <= 0: return
+	
+	show()
 	variables.connect("purple_starbit_collected", self, "collect_coin")
+
+	var new_coins: int = variables.purple_starbits_collected[Singleton.CurrentLevelData.area][0]
+	update_counter(new_coins)
 	
-	if max_purples > 0:
-		visible = true
-		update_required_purples()
-	
-	if required_purples > 0:
-		var new_coins: int = variables.purple_starbits_collected[Singleton.CurrentLevelData.area][0]
-		update_counter(new_coins)
-	else:
+	if required_purples <= 0:
 		slash.visible = false
 		required.visible = false
 
@@ -47,7 +48,7 @@ func collect_coin(new_coins: int):
 func update_counter(new_coins: int):
 	update_required_purples()
 	
-	if new_coins == required_purples:
+	if required_purples > 0 and new_coins == required_purples:
 		fadeout.play("fadeout")
 	
 	collected.text = str(new_coins).pad_zeros(ZEROES_LENGTH)
