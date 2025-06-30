6de0e7765ba111be46fd8716ef24106c5ffb39f5
aloelucidity <aloelucidi@gmail.com>
2025-06-30 20:23:23 +0200

    fix weird levels list bug
    where did this come from anyways

--- scenes/menu/screen.gd
+++ scenes/menu/screen.gd
@@ -20,4 +20,3 @@ func transition(new_screen_name: String):
 
 func animation_finished(_anim_name: String, new_screen_name: String):
 	emit_signal("screen_change", new_screen_name)
-	emit_signal("screen_change", new_screen_name)
