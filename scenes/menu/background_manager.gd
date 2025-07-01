dc6f53a76e9464e00c2f21d10af7b6d4e928655c
SilverCoin1719 <88743665+SilverCoin1719@users.noreply.github.com>
2025-07-01 11:02:15 -0400

    random background presets

--- scenes/menu/background_manager.gd
+++ scenes/menu/background_manager.gd
@@ -9,14 +9,36 @@ const PARALLAX_OFFSET: int = 160
 const PARALLAX_PALETTE: int = 1
 const SCROLL_SPEED: float = 300.0
 
+const BACKGROUNDS := [
+	[1, 13, 0, 64],
+	[6, 1, 3, 160],
+	[10, 5, 0, 128],
+	[6, 13, 3, 64],
+]
+
+var current_star_coins: int = 100
+var total_star_coins: int = 100
+
 
 func _ready():
+	var preset = get_random_preset()
+	
 	backgrounds.do_auto_scroll = true
 	backgrounds.update_background(
-		BG_ID, 
-		PARALLAX_ID, 
+		preset[0], 
+		preset[1], 
 		Rect2(0, 0, 24, 14), 
-		PARALLAX_OFFSET,
-		PARALLAX_PALETTE,
+		preset[3],
+		preset[2],
 		SCROLL_SPEED
 	)
+
+
+func get_random_preset() -> Array:
+	randomize()
+	
+	var star_coin_percentage: float = current_star_coins/total_star_coins
+	var max_index = round(BACKGROUNDS.size() * star_coin_percentage)
+	var index = round(rand_range(0, max_index - 1))
+	
+	return BACKGROUNDS[index]
