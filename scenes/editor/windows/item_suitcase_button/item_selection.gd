8b60240e343e200cb0a4042589321b0e2762e489
dignityloss <60553204+dignityloss@users.noreply.github.com>
2025-07-15 10:01:45 +0300

    more item suitcase progress

--- scenes/editor/windows/item_suitcase_button/item_selection.gd
+++ scenes/editor/windows/item_suitcase_button/item_selection.gd
@@ -2,13 +2,29 @@ extends ButtonSound
 
 
 var placeable_item: Resource = preload("res://scenes/editor/items/placeable_items/blue_coin.tres")
+var button_pressed: bool
+var hold_timer = 2
 
 signal new_item_selected(placeable_item)
+signal new_favorite_selected(placeable_item)
 
 func _ready():
 	set_item(placeable_item)
-	connect("button_down", self, "button_pressed")
+	connect("pressed", self, "button_pressed")
+	connect("button_down", self, "button_held")
+	connect("button_up", self, "button_released")
 
+func _process(delta):
+	if button_pressed == true:
+		yield(get_tree().create_timer(hold_timer), "timeout")
+		if button_pressed == true:
+			#NOTHING WORKS BUT THIS I DONT KNOW WHY
+			#YOU CAN TRY GET_NODE("%ITEMSUITCASE) IT WONT WORK
+			#YOU CAN TRY $"../PATH TO SUITCASE" AND IT WONT WORK
+			#I HATE THIS ENGINE
+			get_parent().get_parent().get_parent().get_parent().new_favorite_selected(placeable_item, self)
+			button_pressed = false
+		
 
 func set_item(value: PlaceableItem):
 	placeable_item = value
@@ -20,3 +36,9 @@ func set_item(value: PlaceableItem):
 func button_pressed():
 	connect("new_item_selected", get_tree().get_current_scene(), "new_item_selected")
 	emit_signal("new_item_selected", placeable_item)
+	
+func button_held():
+	button_pressed = true
+	
+func button_released():
+	button_pressed = false
