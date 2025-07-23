264ef76f9d318399b3c0ed9a72b50ce38c1fcb92
SilverCoin1719 <88743665+SilverCoin1719@users.noreply.github.com>
2025-07-23 10:20:38 -0400

    enum for tile variants

--- scenes/editor/items/placeable_tile.gd
+++ scenes/editor/items/placeable_tile.gd
@@ -2,7 +2,9 @@ class_name PlaceableTile
 extends PlaceableItem
 
 
+enum TileVariants {Normal, HalfTile, SlopeLeft, SlopeRight}
+
 export var tileset_id := 0
-export var tile_id := 0
+export(TileVariants) var tile_id := 0
 
 
