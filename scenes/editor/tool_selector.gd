f6ff329094319f87bf2f0345768bb3edec30c9b2
dignityloss <60553204+dignityloss@users.noreply.github.com>
2025-07-30 21:10:49 +0300

    quick fix

--- scenes/editor/tool_selector.gd
+++ scenes/editor/tool_selector.gd
@@ -3,8 +3,9 @@ extends PanelContainer
 export(NodePath) var editor_path
 onready var editor = get_node(editor_path)
 
-onready var tile_lock: Button = $"%TileLock"
-onready var rectangle_fill: Button = $"%RectangleFill"
+onready var tile_lock: Button = $"%ObjectLock"
+onready var rectangle_fill: Button = $"%TileFill"
+onready var tile_rect_fill = $"%TileRectFill"
 onready var item_tools: VBoxContainer = $"%ItemTools"
 
 signal tool_picked(tool_name)
@@ -21,21 +22,20 @@ func on_button_pressed(button):
 	
 func detect_tool_buttons(button: Button):
 	yield(get_tree(), "idle_frame")
-	print(button.name)
-	if "Tile" in button.name && "Object" in editor.tool_manager.current_tool.name:
-		button.hide()
-		return
+	if "Object" in editor.tool_manager.current_tool.name:
+		tile_lock.show()
+		rectangle_fill.hide()
+		tile_rect_fill.hide()
 	else:
-		button.show()
-	if "Object" in button.name && "Tile" in editor.tool_manager.current_tool.name:
-		button.hide()
-		return
-	else:
-		button.show()
+		tile_lock.hide()
+		rectangle_fill.show()
+		tile_rect_fill.show()
+
 
 func _on_Tools_tool_changed():
 	for button in item_tools.get_children():
 		if button is Button:
+			detect_tool_buttons(button)
 			if button.name in editor.tool_manager.current_tool.name:
 				button.pressed = true
 			else:
