afadf2ede056145a2f44e4cb5e639cf4add68211
dignityloss <60553204+dignityloss@users.noreply.github.com>
2025-08-04 13:31:13 +0300

    my saving? auto

--- scenes/oldeditor/autosave.gd
+++ scenes/oldeditor/autosave.gd
@@ -1,24 +1,36 @@
+class_name Autosave
 extends Node
 
-var autosave_interval = 60 * 5
-var timer = 0.0
+const AUTOSAVE_FOLDER = "user://autosaves/"
+
+var intervals: Dictionary = {
+	-1: "Never",
+	300: "5 Minutes",
+	900: "15 Minutes",
+	1800: "30 Minutes",
+	3600: "1 Hour"
+}
+
+var interval: int = 900
+var timer: float = 0
+
+signal autosaved
 
 func _ready():
-	timer = autosave_interval
-
+	timer = LocalSettings.load_setting("General", "autosave_interval", 900)
+	
 func _physics_process(delta):
-	# again putting this here because dunno where else
-	OS.set_window_title("Super Mario 127 (FPS: " + str(Engine.get_frames_per_second()) + ")")
+	if timer > 0:
+		timer -= delta
+	elif timer != -1:
+		autosave()
+		timer = interval
 
-	timer -= delta
-	if timer <= 0:
-		timer = autosave_interval
-		save()
-
-func save():
-	if "mode" in get_tree().current_scene and get_tree().current_scene.mode == 1:
-		var saved_file = File.new()
-		saved_file.open("user://autosave.txt", File.WRITE)
-		saved_file.store_line(Singleton.CurrentLevelData.level_data.get_encoded_level_data())
-		saved_file.close()
-		print("Autosaved level")
+func autosave():
+	var file_name: String = "%s_%s" % [Singleton.CurrentLevelData.level_id, round(Time.get_unix_time_from_system())]
+	
+	var file := File.new()
+	file.open(AUTOSAVE_FOLDER + file_name, File.WRITE)
+	file.store_string(Singleton.CurrentLevelData.level_data.get_encoded_level_data())
+	file.close()
+	emit_signal("autosaved")
