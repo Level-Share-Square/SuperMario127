a337b76f0d5b12c6c400a7fa688418f04c182035
CassStar <163167577+CassStar@users.noreply.github.com>
2025-08-27 09:20:35 -0500

    Recolourable Shine Particles
    Makes the shine sparkle particles match custom shine colours instead of just turning white

--- scenes/actors/objects/shine/shine_rays_recolorable.gd
+++ scenes/actors/objects/shine/shine_rays_recolorable.gd
@@ -2,11 +2,10 @@ tool
 extends Node2D
 
 
-export var ray_path: NodePath
-onready var ray_sprite: Node2D = get_node(ray_path)
+export var shine_path: NodePath
+onready var shine_sprite: GameObject = get_node(shine_path)
 
 
 func _process(_delta):
-	if is_instance_valid(ray_sprite):
-		transform = ray_sprite.transform
-		modulate.a = ray_sprite.modulate.a
+	if is_instance_valid(shine_sprite) and shine_sprite.color != shine_sprite.NORMAL_COLOR:
+		modulate = shine_sprite.color
