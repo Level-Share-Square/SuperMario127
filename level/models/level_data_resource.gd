86b6ddcf282b5ece8606f2c4fbf29e8af592f3a7
SilvererCoin <88743665+SilvererCoin@users.noreply.github.com>
2026-07-17 00:30:33 -0400

    Add Mutexes to LevelDataResource class
    - Adds a mutex to the LevelDataResource class that can be used to lock down modifying the resource when using threads

--- level/models/level_data_resource.gd
+++ level/models/level_data_resource.gd
@@ -1,2 +1,19 @@
 class_name LevelDataResource
 extends Resource
+
+
+var _mutex: Mutex
+
+
+func _init() -> void:
+	_mutex = Mutex.new()
+
+
+# For threading. Lock the object before reading and writing it's data.
+func lock() -> void:
+	_mutex.lock()
+
+
+# For threading. Unlock the object after reading and writing it's data.
+func unlock() -> void:
+	_mutex.unlock()
