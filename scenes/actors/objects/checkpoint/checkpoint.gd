6fedd555b715bbb8f9345edbaa5e897b758a34d7
SilvererCoin <88743665+SilvererCoin@users.noreply.github.com>
2026-07-24 03:29:58 -0400

    Checkpoint GameObject callbacks
    - checkpoint object now uses GameObject callbacks

--- scenes/actors/objects/checkpoint/checkpoint.gd
+++ scenes/actors/objects/checkpoint/checkpoint.gd
@@ -14,23 +14,21 @@ var save_switch_state := true
 var spawn_offset := Vector2(0,0)
 var id = 0
 
+
 func _set_properties():
 	savable_properties = ["save_water_level", "spawn_offset", "save_switch_state", "id"]
 	editable_properties = ["save_water_level", "save_switch_state", "spawn_offset"]
-	
+
+
 func _set_property_values():
 	set_property("save_water_level", save_water_level, true)
 	set_property("spawn_offset", spawn_offset, true)
 	set_property("save_switch_state", save_switch_state, true)
 
-func _ready():
-	if is_preview: return
-	
-	if mode != 1:
-		display.visible = false
-		var _connect = use_area.connect("body_entered", self, "set_checkpoint")
-	else:
-		display.visible = true
+
+func _object_ground_ready():
+	display.visible = false
+	var _connect = use_area.connect("body_entered", self, "set_checkpoint")
 	
 #	CurrentLevelData.set_checkpoint_ids()
 #	id = level_object.get_ref().properties[9]
@@ -39,13 +37,30 @@ func _ready():
 	
 	CurrentLevelData.vars.checkpoints.append([id, self])
 
-func _physics_process(delta):
+
+func _editor_ready():
+	display.visible = true
+
+
+func _object_process(delta: float):
+	update_ring_particles(delta)
+
+
+func _editor_process(delta: float):
+	display.global_scale = Vector2.ONE
+	update_ring_particles(delta)
+
+
+func update_ring_particles(delta: float):
+	if not $"%VisibilityEnabler2D".is_on_screen():
+		return
+	
 	var sprite = $Rotation/RotationRight
 	var particles = $Rotation/RotationRight/Particles
 	
 	particles.texture = used_sparkles if is_used else normal_sparkles
 #	sprite.reset_physics_interpolation()
-	sprite.rotate(deg2rad(8))
+	sprite.rotate(deg2rad(8) * delta * 60)
 	
 	sprite.scale = sprite.scale.move_toward(Vector2(1, 1), delta * 4) if !is_used else sprite.scale.move_toward(Vector2(1.15, 1.15), delta * 8)
 
@@ -54,9 +69,10 @@ func _physics_process(delta):
 	
 	particles2.texture = used_sparkles if is_used else normal_sparkles
 #	sprite2.reset_physics_interpolation()
-	sprite2.rotate(deg2rad(-8))
+	sprite2.rotate(deg2rad(-8) * delta * 60)
 	
 	sprite2.scale = sprite2.scale.move_toward(Vector2(1, 1), delta * 4) if !is_used else sprite2.scale.move_toward(Vector2(1.15, 1.15), delta * 8)
+
 
 func set_checkpoint(body):
 	if is_used or !enabled:
