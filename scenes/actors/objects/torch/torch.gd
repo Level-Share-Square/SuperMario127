53025a8e21f24840267bd8d9124e065bf21d30fa
SilvererCoin <88743665+SilvererCoin@users.noreply.github.com>
2026-07-28 21:32:07 -0400

    Torch property updates

--- scenes/actors/objects/torch/torch.gd
+++ scenes/actors/objects/torch/torch.gd
@@ -3,7 +3,6 @@ extends GameObject
 
 var is_background = false
 var color = Color(1, 1, 0)
-var next_layer = false
 const light_color = Color(0.87, 0.65, 0.05)
 export(Array, Texture) var palette_textures
 
@@ -13,27 +12,20 @@ onready var sprite2 : AnimatedSprite = $AnimatedSprite/RecolorableSprite
 
 
 func _set_properties():
-	savable_properties = ["is_background", "color", "next_layer"]
-	editable_properties = ["color", "next_layer"]
+	savable_properties = ["color"]
+	editable_properties = ["color"]
 
 
 func _set_property_values(): 
 	set_property("is_background", is_background, true)
 	set_property("color", color, true)
-	set_property("next_layer", next_layer, true)
 
 
 func _ready():
-	if is_background:
-		set_property("layer", 1, true)
-		update_layer()
-		set_property("is_background", false, true)
-		
 	if mode == 1:
 		# warning-ignore: unused_variable
 		connect("property_changed", self, "update_property")
 	
-	update()
 	update_light_layer()
 
 
@@ -41,10 +33,6 @@ func update_property(key: String, value):
 	match key:
 		"visible", "enabled":
 			update()
-		
-		"layer":
-			update_layer()
-			update_light_layer()
 
 
 func _process(delta):
@@ -58,7 +46,6 @@ func _process(delta):
 		var color_0 = color
 		var color_1 = color
 		
-
 		color_0.s /= 1.5
 		color_0.v *= 3
 		
@@ -76,15 +63,6 @@ func update():
 	visible = true
 
 
-func update_layer():
-	if next_layer:
-		layer_shift = LevelShared.layer_spacing
-	else:
-		layer_shift = 0
-	
-	.update_layer()
-
-
 func update_light_layer():
 	light.range_z_max = z_index
-	light.range_z_min = light.range_z_max - (LevelShared.layer_spacing * 1.5)
+	light.range_z_min = light.range_z_max - (LevelLayer.LAYER_Z_SPACING * 1.5)
