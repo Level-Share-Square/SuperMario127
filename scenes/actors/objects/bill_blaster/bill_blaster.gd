1c27c8c6b7d71a2e2906cb4b558b1433d81cde2d
dignityloss <abdo_essam63@outlook.com>
2026-08-01 19:34:22 +0300

    i guess bro
    reorder bill blaster property registration

--- scenes/actors/objects/bill_blaster/bill_blaster.gd
+++ scenes/actors/objects/bill_blaster/bill_blaster.gd
@@ -19,11 +19,11 @@ var force_direction := 0
 func _register_properties():
 	register_property(4, "chase", chase)
 	register_property(5, "speed", speed)
-	register_property(6, "offset", offset)
-	register_property(7, "color", color)
-	register_property(8, "wait_time", wait_time)
-	register_property(9, "invincible", invincible)
-	register_property(10, "force_direction", force_direction)
+	register_property(10, "offset", offset)
+	register_property(6, "color", color)
+	register_property(7, "wait_time", wait_time)
+	register_property(8, "invincible", invincible)
+	register_property(9, "force_direction", force_direction)
 #	set_property_menu("force_direction", ["option", 3, -1, ['Face Player', 'Right', 'Left']])
 
 
