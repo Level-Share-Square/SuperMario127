571f2f478ce6f63b69467ce535db819799656c34
dignityloss <abdo_essam63@outlook.com>
2026-08-02 22:06:23 +0300

    new layer changer icons

--- scenes/actors/objects/layer_changer/layer_changer.gd
+++ scenes/actors/objects/layer_changer/layer_changer.gd
@@ -104,6 +104,7 @@ func update_layer(body):
 		var layer: LevelLayer = shared.get_layer(layer_uuid)
 		if layer is LevelParallaxLayer:
 			layer.set_parallax_distance(parallax_distance)
+#		print(move_to_index)
 		layer.set_layer_modulate(Color.white, opacity)
 		if move_to_index != -1:
 			shared.move_layer(layer, move_to_index)
