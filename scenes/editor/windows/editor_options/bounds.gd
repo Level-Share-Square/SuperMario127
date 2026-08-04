6346a7ee5baba97f4a3ab61512985f2060d8d664
dignityloss <abdo_essam63@outlook.com>
2026-08-04 15:51:49 +0300

    area property editing action + level bounds

---
+++ scenes/editor/windows/editor_options/bounds.gd
@@ -0,0 +1,71 @@
+extends HBoxContainer
+class_name AreaBoundsEditor
+
+enum ChangeType {ADD, SUBTRACT}
+
+const X_LABEL_PREFIX: String = "X: %d"
+const Y_LABEL_PREFIX: String = "Y: %d"
+
+onready var top = $"%Top"
+onready var left = $"%Left"
+onready var x_label = $"%XLabel"
+onready var y_label = $"%YLabel"
+onready var right = $"%Right"
+onready var bottom = $"%Bottom"
+onready var increment = $"%Increment"
+
+onready var editor = get_tree().current_scene
+onready var shared = editor.get_shared_node()
+onready var camera = editor.get_node("%EditorCamera")
+
+var area_rect: Rect2
+var area: AreaData
+
+func _ready():
+	var containers = [top, left, right, bottom]
+	for container in containers:
+		container.get_node("Add").connect("pressed", self, "change_bounds", [container.name.to_lower(), ChangeType.ADD])
+		container.get_node("Subtract").connect("pressed", self, "change_bounds", [container.name.to_lower(), ChangeType.SUBTRACT])
+		
+	area = CurrentLevelData.current_area
+	area_rect = area.header.bounds
+	update_values()
+	
+	yield(editor, "ready")
+	editor.action_manager.connect("action", self, "update_values")
+	editor.action_manager.connect("undo", self, "update_values")
+	editor.action_manager.connect("redo", self, "update_values")
+		
+func change_bounds(side: String, type: int):
+	var amount = increment.value
+	if type == ChangeType.SUBTRACT: amount *= -1
+	
+	area_rect = area.header.bounds
+	match side:
+		"top":
+			area_rect = area_rect.grow_individual(0, amount, 0, 0)
+		"left":
+			area_rect = area_rect.grow_individual(amount, 0, 0, 0)
+		"right":
+			area_rect = area_rect.grow_individual(0, 0, amount, 0)
+		"bottom":
+			area_rect = area_rect.grow_individual(0, 0, 0, amount)
+			
+	print(area_rect)
+	action()
+	
+	update_values()
+	
+func action() -> void:
+	var action := ChangeAreaAction.new()
+	action.property = "bounds"
+	action.id = CurrentLevelData.area_id
+	action.new_value = area_rect
+	editor.action_manager.commit_action(action)
+	
+func update_values():
+	area_rect = area.header.bounds
+	x_label.text = X_LABEL_PREFIX % area_rect.size.x
+	y_label.text = Y_LABEL_PREFIX % area_rect.size.y
+	shared.update_tilemaps()
+	camera.update_limits(area.header)
