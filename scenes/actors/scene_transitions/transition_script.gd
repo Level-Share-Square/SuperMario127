3dd0ae0a85b4e6113e4b7625bacc38c9f6e86de7
dignityloss <abdo_essam63@outlook.com>
2026-08-06 00:11:41 +0300

    fix time score resetting after every transition

--- scenes/actors/scene_transitions/transition_script.gd
+++ scenes/actors/scene_transitions/transition_script.gd
@@ -29,7 +29,6 @@ func reload_scene(transition_in_tex = cutout_circle, transition_out_tex = cutout
 		yield(do_transition_animation(transition_in_tex, transition_time, TRANSITION_SCALE_UNCOVER, TRANSITION_SCALE_COVERED, volume_multiplier, volume_multiplier / 4, false, false), "completed")
 		
 		CurrentLevelData.pause_time_score()
-		CurrentLevelData.reset_time_score()
 		get_tree().reload_current_scene()
 		if new_area != -1 and new_area != CurrentLevelData.area_id:
 			CurrentLevelData.switch_to_area(new_area)
