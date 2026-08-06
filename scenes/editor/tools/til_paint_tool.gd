283c0811c9cefc835eff2798fa4d9d6d62c9013b
aloelucidity <106764798+aloelucidity@users.noreply.github.com>
2026-08-06 15:31:03 +0200

    bresenham is back at red lobster

--- scenes/editor/tools/til_paint_tool.gd
+++ scenes/editor/tools/til_paint_tool.gd
@@ -5,50 +5,73 @@ var last_mouse_tile: Vector2
 var mouse_input: int = -1
 var used_tiles: Array = []
 var undo_tiles: Dictionary = {}
+var is_erasing: bool = false
+
 
 func _click_left(_event: InputEvent, _world_pos: Vector2) -> void:
-#	print(shared.get_tile(9999, 9999, 2))
 	if mouse_input > -1:
 		return
-	
-	if Input.is_action_just_pressed("place"):
-		last_mouse_tile = get_mouse_tile_pos()
-		editor.tile_buffer.modulate = shared.layer_dictionary[editor.layer].layer_tint
-		draw_tile(last_mouse_tile)
-		mouse_input = 0
+	is_erasing = tool_manager.is_erasing
+	click()
 
 
 func _click_left_released(_event: InputEvent, _world_pos: Vector2) -> void:
-	if editor.selected_item is PlaceableTile:
-		if Input.is_action_just_released("place") and mouse_input == 0:
-			finalize_placement()
-			mouse_input = -1
+	is_erasing = tool_manager.is_erasing
+	click_released()
 
 
-func _mouse_movement(_event: InputEvent, world_pos: Vector2) -> void:
+func _click_right(event: InputEvent, world_pos: Vector2) -> void:
+	if mouse_input > -1:
+		return
+	is_erasing = not tool_manager.is_erasing
+	click()
+
+
+func _click_right_released(event: InputEvent, world_pos: Vector2) -> void:
+	is_erasing = not tool_manager.is_erasing
+	click_released()
+
+
+func _mouse_movement(event: InputEvent, world_pos: Vector2) -> void:
 	if editor.selected_item is PlaceableTile:
 		var mouse_tile: Vector2 = get_mouse_tile_pos()
 		
-#		var line: = line_util.get_line(last_mouse_tile, mouse_tile)
-#
-#		if Input.is_action_pressed("place") and mouse_input == 0:
-#			for point in line:
-#				draw_tile(point)
-#
-#		last_mouse_tile = mouse_tile
+		var line: = line_util.get_line(last_mouse_tile, mouse_tile)
+
+		if mouse_input == 0:
+			for point in line:
+				draw_tile(point)
+
+		last_mouse_tile = mouse_tile
 		
-		if Input.is_action_pressed("place") and mouse_input == 0 and mouse_tile != last_mouse_tile:
+		if mouse_input == 0 and mouse_tile != last_mouse_tile:
 			draw_tile(mouse_tile)
 			last_mouse_tile = mouse_tile
-		
-#	print(last_mouse_tile)
+
+
+func click() -> void:
+	last_mouse_tile = get_mouse_tile_pos()
+	editor.tile_buffer.modulate = shared.layer_dictionary[editor.layer].layer_tint
+	draw_tile(last_mouse_tile)
+	mouse_input = 0
+
+
+func click_released() -> void:
+	if editor.selected_item is PlaceableTile:
+		if mouse_input == 0:
+			finalize_placement()
+			mouse_input = -1
 
 
 func draw_tile(pos: Vector2) -> void:
 	var item = editor.selected_item
 	used_tiles.append(pos)
 	undo_tiles.get_or_add(pos, shared.get_tile(pos.x, pos.y, editor.layer))
-	shared.set_tile(pos.x, pos.y, editor.layer, item.tileset_id, item.tile_id, item.palette)
+	shared.set_tile(pos.x, pos.y, editor.layer, 
+		0 if is_erasing else item.tileset_id, 
+		0 if is_erasing else item.tile_id, 
+		0 if is_erasing else item.palette
+	)
 
 
 func finalize_placement() -> void:
@@ -58,9 +81,9 @@ func finalize_placement() -> void:
 	var action := PlaceTilesAction.new()
 	action.shared = shared
 	action.layer = editor.layer
-	action.tileset_id = editor.selected_item.tileset_id
-	action.tile_id = editor.selected_item.tile_id
-	action.palette = editor.selected_item.palette
+	action.tileset_id = 0 if is_erasing else editor.selected_item.tileset_id
+	action.tile_id = 0 if is_erasing else editor.selected_item.tile_id
+	action.palette = 0 if is_erasing else editor.selected_item.palette
 	action.do_tiles = used_tiles
 	action.undo_tiles = undo_tiles.duplicate()
 	action.has_margins = shared.get_layer(editor.layer).tile_map_manager.has_margins
--- util/new/line_util.gd
+++ util/new/line_util.gd
@@ -9,9 +9,9 @@ static func get_line(start: Vector2, end: Vector2) -> PoolVector2Array:
 	# adding one, so it doesr't stop drawing too early
 	for step in range(steps + 1):
 		var time: float = step / steps
-		points.append(Vector2(
-			lerp(Vector2(start), Vector2(end), time))
-		)
+		var current_point: Vector2 = lerp(Vector2(start), Vector2(end), time).round()
+		if points.empty() or points[points.size() - 1] != current_point:
+			points.append(current_point)
 	
 	points.append(end)
 	return points
