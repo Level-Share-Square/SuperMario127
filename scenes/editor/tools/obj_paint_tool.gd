68e0f2eae2f5236686831bed5c7a771d56f46a85
dignityloss <abdo_essam63@outlook.com>
2026-08-07 21:45:42 +0300

    drag objects + fix autotiling updating bug

--- scenes/editor/tools/obj_paint_tool.gd
+++ scenes/editor/tools/obj_paint_tool.gd
@@ -1,8 +1,16 @@
 extends EditorTool
 
+const HOLD_TIME: float = 0.07
 
 var last_mouse_tile: Vector2
 var is_erasing: bool
+
+var is_dragging: bool = false
+
+var old_pos: Vector2
+var pos_offset: Vector2
+
+var hovered_object: GameObject
 
 signal objects_selected(objects)
 
@@ -24,18 +32,47 @@ func _click(world_pos: Vector2) -> void:
 		if editor.selected_objects.empty() && editor.hovered_objects.empty():
 			place_object(world_pos)
 		elif !editor.hovered_objects.empty():
-			var closest_object = editor.hovered_objects.values()[0]
-			for object in editor.hovered_objects.values():
-				if Vector2(abs(object.global_position.x - get_mouse_pos().x), abs(object.global_position.y - get_mouse_pos().y)) < closest_object.global_position:
-					closest_object = object
-				emit_signal("objects_selected", [closest_object])
+			hovered_object = find_closest_object(editor.hovered_objects.values())
+			old_pos = hovered_object.global_position
+			pos_offset = old_pos - get_mouse_pos()
 		else:
 			emit_signal("objects_selected", [])
 			pass
 	else:
 		for object in editor.hovered_objects.values():
 			erase_object(object)
+			
+func _mouse_movement(event, mouse_pos):
+	if hovered_object:
+		is_dragging = true
+			
+func _process(delta):
+	if is_dragging and hovered_object:
+		if get_mouse_pos().is_equal_approx(old_pos): return
+		hovered_object.global_position = get_mouse_pos() + pos_offset
+			
+func _click_left_released(event, mouse_pos):
+	if hovered_object:
+		if is_dragging:
+			is_dragging = false
+			change_property(hovered_object, "position", hovered_object.global_position, old_pos)
+		else:
+			emit_signal("objects_selected", [hovered_object])
+		hovered_object = null
 
+func find_closest_object(objects: Array) -> GameObject:
+	var closest_object: GameObject = objects[0]
+	var mouse_pos: Vector2 = get_mouse_pos()
+	var min_dist: float = INF
+	
+	for object in objects:
+		var dist: float = mouse_pos.distance_squared_to(object.global_position)
+		
+		if dist < min_dist:
+			closest_object = object
+			min_dist = dist
+
+	return closest_object
 
 func place_object(pos: Vector2):
 	if shared.get_object_at_position(Vector2(round(pos.x), round(pos.y)), editor.layer):
@@ -64,3 +101,21 @@ func erase_object(object: GameObject):
 	action.layer = editor.layer
 	action.object = object
 	editor.action_manager.commit_action(action)
+	
+func change_property(object, property: String, new_value, old_value):
+	var properties: Dictionary = setup_properties(property, new_value, old_value)
+	var action := ChangePropertyAction.new()
+	action.object = object
+	action.changed_properties = properties["changed_properties"]
+	action.original_properties = properties["original_properties"]
+	editor.action_manager.commit_action(action)
+
+func setup_properties(property: String, new_value, old_value) -> Dictionary:
+	var properties: Dictionary = {
+		"changed_properties": {},
+		"original_properties": {}
+	}
+	properties["changed_properties"] = {property: new_value}
+	properties["original_properties"] = {property: old_value}
+	return properties
+
