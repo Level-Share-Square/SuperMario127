175fa084689c3b021fd622972a6450e6c8d106a2
aloelucidity <106764798+aloelucidity@users.noreply.github.com>
2026-08-12 21:43:28 +0200

    layer dropdown can shrink when renaming layers

--- scenes/editor/dropdown_parent.gd
+++ scenes/editor/dropdown_parent.gd
@@ -4,6 +4,7 @@ onready var editor: Editor = owner
 onready var parent: Control = get_parent()
 onready var dropdown: Control = get_child(0)
 onready var layer_dropdown = $"%LayerDropdown"
+var resizing: bool = false
 
 func _ready() -> void:
 	parent.connect("resized", self, "dropdown_resized")
@@ -16,6 +17,12 @@ func _ready() -> void:
 	editor.action_manager.connect("action", self, "dropdown_resized")
 
 func dropdown_resized() -> void:
-	rect_min_size.x = max(dropdown.rect_size.x, parent.rect_size.x)
+	if resizing: return
+	resizing = true
+	var dropdown_min: float = dropdown.get_minimum_size().x
+	hide()
+	rect_min_size.x = max(dropdown_min, parent.get_minimum_size().x)
 	rect_size.x = rect_min_size.x
-	dropdown.rect_size.x = max(dropdown.rect_size.x, rect_min_size.x)
+	dropdown.rect_size.x = rect_min_size.x
+	show()
+	resizing = false
