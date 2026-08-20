86336301fce263e74240252039374f9eb9f081c2
aloelucidity <aloelucidi@gmail.com>
2026-08-20 09:17:43 +0200

    fix new menu using old menu script

--- scenes/menu/shine_select/shine_sprite.gd
+++ scenes/menu/shine_select/shine_sprite.gd
@@ -1,11 +1,15 @@
 extends AnimatedSprite
 
-onready var shine_outline : AnimatedSprite = $ShineOutline
+onready var shine_recolorable : AnimatedSprite = $ShineRecolorable
 onready var animation_player : AnimationPlayer = $AnimationPlayer
 
-const frames_normal : Resource = preload("res://scenes/actors/objects/shine/frames_normal.tres")
-const frames_recolorable : Resource = preload("res://scenes/actors/objects/shine/frames_recolorable.tres")
-const frames_collected : Resource = preload("res://scenes/actors/objects/shine/frames_collected.tres")
+const FRAMES_NORMAL: Resource = preload("res://scenes/actors/objects/shine/frames_normal.tres")
+const FRAMES_RECOLORABLE: Resource = preload("res://scenes/actors/objects/shine/frames_recolorable.tres")
+const FRAMES_COLLECTED: Resource = preload("res://scenes/actors/objects/shine/frames_collected.tres")
+
+const FRAMES_POCKET: Resource = preload("res://scenes/actors/objects/shine/frames_pocket.tres")
+const FRAMES_POCKET_RECOLORABLE: Resource = preload("res://scenes/actors/objects/shine/frames_pocket_recolorable.tres")
+const FRAMES_POCKET_COLLECTED: Resource = preload("res://scenes/actors/objects/shine/frames_pocket_collected.tres")
 
 const NORMAL_COLOR := Color(1, 1, 0)
 const WHITE_COLOR := Color(1, 1, 1) # because apparently this needs to be const
@@ -15,24 +19,22 @@ var is_flipped : bool = false # for non-kickout shines
 
 func start_animation() -> void:
 	play()
-	shine_outline.play()
+	shine_recolorable.play()
+	flip_h = is_flipped
+	shine_recolorable.flip_h = is_flipped
 
 func start_pressed_animation() -> void:
 	var animation = "selected" if selected else "disappear"
 	animation_player.play(animation)
 
 func make_blue() -> void:
-	frames = frames_collected
+	frames = FRAMES_POCKET_COLLECTED if is_flipped else FRAMES_COLLECTED
 
 func set_color(color : Color) -> void:
+	frames = FRAMES_POCKET if is_flipped else FRAMES_NORMAL
 	if color != NORMAL_COLOR:
-		self_modulate = color
-		frames = frames_recolorable
+		shine_recolorable.show()
+		shine_recolorable.self_modulate = color
+		shine_recolorable.frames = FRAMES_POCKET_RECOLORABLE if is_flipped else FRAMES_RECOLORABLE
 	else:
-		self_modulate = WHITE_COLOR
-		frames = frames_normal
-
-func _ready():
-	if is_flipped:
-		flip_h = true
-		shine_outline.flip_h = true
+		shine_recolorable.hide()
