0917ea407d656dab862d7878d283472c30e39e45
dignityloss <abdo_essam63@outlook.com>
2026-08-23 13:51:38 +0300

    tags galore

--- level/level_tags.gd
+++ level/level_tags.gd
@@ -4,6 +4,9 @@ extends LevelDataResource
 export var teleport_tags: Array
 export var dialogue_tags: Array
 export var liquid_tags: Array
+export var key_tags: Array
+
+var key_object_map: Dictionary = {}
 
 func get_teleport_args() -> Dictionary:
 	var args: Dictionary = {}
@@ -23,7 +26,22 @@ func get_liquid_args() -> Dictionary:
 		args[tag] = tag
 	return args
 
-func _init(_teleport_tags: Array = [], _dialogue_tags: Array = [], _liquid_tags: Array = []):
+func get_key_args() -> Dictionary:
+	var args: Dictionary = {}
+	for tag in key_tags:
+		args[tag] = tag
+	return args
+
+func find_key_index(id: String, key_data: KeyData) -> int:
+	var i: int = 0
+	if not key_object_map.has(id): return -1
+	for data in key_object_map[id]:
+		if key_data.is_equal(data):
+			return i
+	return -1
+
+func _init(_teleport_tags: Array = [], _dialogue_tags: Array = [], _liquid_tags: Array = [], _key_tags: Array = []):
 	teleport_tags = _teleport_tags
 	dialogue_tags = _dialogue_tags
 	liquid_tags = _liquid_tags
+	key_tags = _key_tags
---
+++ scenes/actors/objects/key/key_data.gd
@@ -0,0 +1,18 @@
+extends Resource
+class_name KeyData
+
+export var tag: String
+export var color: Color
+export var visible: bool
+
+func _init(_tag: String = "", _color := Color.yellow, _visible: bool = true):
+    tag = _tag
+    color = _color
+    visible = _visible
+
+func is_equal(other: KeyData):
+    if (tag == other.tag and
+        color == other.color and
+        visible == other.visible):
+            return true
+    return false
