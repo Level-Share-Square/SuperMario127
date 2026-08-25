d528ac4bb13a9658c234789d44ae026a5ab08f27
aloelucidity <106764798+aloelucidity@users.noreply.github.com>
2026-08-25 22:49:00 +0200

    updated crystal sprites

--- scenes/actors/objects/big_tree/decoration.gd
+++ scenes/actors/objects/big_tree/decoration.gd
@@ -3,9 +3,16 @@ extends GameObject
 
 export var custom_preview_position = Vector2(70, 170)
 export(Array, Texture) var palette_textures
+export var zero_is_default: bool = false
 
 func _ready():
 	preview_position = custom_preview_position
-	
-	if palette != 0:
-		$Sprite.texture = palette_textures[palette - 1]
+	var _connect = connect("property_changed", self, "update_property")
+	update_property("palette", palette)
+
+func update_property(key: String, value):
+	if key == "palette":
+		if zero_is_default:
+			$Sprite.texture = palette_textures[value]
+		elif value != 0:
+			$Sprite.texture = palette_textures[value - 1]
