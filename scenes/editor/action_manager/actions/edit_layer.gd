94829bc5a42577eda4782999a8f917c193df8f55
cap39 <buzzkill619@gmail.com>
2026-08-28 12:24:24 -0500

    changing color disables autoset tint

--- scenes/editor/action_manager/actions/edit_layer.gd
+++ scenes/editor/action_manager/actions/edit_layer.gd
@@ -8,11 +8,19 @@ var property: String
 var new_value
 var old_value
 
+var layer_tint_and_autoset_tint_toggled: bool = false
+
 func change_property(value) -> void:
 	var metadata: LayerMetadata = shared.get_layer_at(layer_index).layer_data.layer_metadata
 	old_value = metadata[property]
 	shared.edit_layer(metadata.layer_uuid, property, value)
-	
+	if(property == "layer_tint"):
+		if(layer_tint_and_autoset_tint_toggled):
+			shared.edit_layer(metadata.layer_uuid, "autoset_tint", true)
+			layer_tint_and_autoset_tint_toggled = false
+		else:
+			shared.edit_layer(metadata.layer_uuid, "autoset_tint", false)
+			layer_tint_and_autoset_tint_toggled = true
 
 func _do():
 	change_property(new_value)
