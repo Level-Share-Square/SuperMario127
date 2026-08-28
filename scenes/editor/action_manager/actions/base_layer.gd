7ea66ad614353778850d3a42caec87e74242c378
cap39 <buzzkill619@gmail.com>
2026-08-27 20:23:49 -0500

    Fix autoset tint

--- scenes/editor/action_manager/actions/base_layer.gd
+++ scenes/editor/action_manager/actions/base_layer.gd
@@ -13,6 +13,7 @@ func add_layer(layer_data: LayerData, layer_index: int = -1, ground: bool = true
 		)
 		layer_data.layer_metadata.order = shared.get_layer(shared.layers.back()).layer_data.layer_metadata.order + 1
 		layer_data.layer_metadata.is_ground = ground
+		layer_data.layer_metadata.autoset_tint = true
 		layer_data.layer_metadata.layer_name = layer_data.layer_metadata.layer_name % (shared.layers.size() + 1)
 	
 	layer_index = layer_index if layer_index != -1 else shared.layers.size()
--- scenes/shared/layers/parallax_layer/parallax_layer.gd
+++ scenes/shared/layers/parallax_layer/parallax_layer.gd
@@ -7,10 +7,10 @@ onready var parallax_scroll: ParallaxScroll = $"%ParallaxScroll"
 const AUTOSET_DARKEN_GROWTH = 0.003
 
 func load_in(layer_data: LayerData):
-	.load_in(layer_data)
-	
 	parallax_scroll.set_parallax_distance(layer_data.layer_metadata.parallax_distance)
 	parallax_scroll.set_lock_axis(layer_data.layer_metadata.lock_axis)
+	
+	.load_in(layer_data)
 
 
 func set_parallax_distance(s_parallax_distance: float) -> void:
@@ -27,7 +27,7 @@ func set_lock_axis(s_lock_axis: int) -> void:
 
 
 func _modulate_autoset() -> Color:
-	if parallax_scroll.parallax_distance > 0:
+	if parallax_scroll.parallax_distance >= 0:
 		return Color.white.darkened(0.5 + 0.45 * (1 - pow(exp(1), (- AUTOSET_DARKEN_GROWTH * parallax_scroll.parallax_distance))))
 	else:
 		return Color.white
