3bd258be88bcc15016e1be301f36edb162212083
dignityloss <abdo_essam63@outlook.com>
2026-08-29 17:23:16 +0300

    Update mario.gd

--- scenes/actors/mario/mario.gd
+++ scenes/actors/mario/mario.gd
@@ -1429,6 +1429,9 @@ func check_liquid(liquid_type) -> bool:
 func update_layer_info():
 	get_parent().remove_child(self)
 	layer.get_ref().add_child(self)
+	
+	var player = get_tree().current_scene
+	player.character = player.get_path_to(self)
 
 func handle_liquids(liquid_areas, delta):
 	if is_instance_valid(state):
