24dcbef6cf5dbfbc650961a420696f42541d7974
Laith Hijazi <90116898+Gapva@users.noreply.github.com>
2026-09-03 19:17:00 -0400

    feat(actions/add_layer): add insert_index attribute

--- scenes/editor/action_manager/actions/add_layer.gd
+++ scenes/editor/action_manager/actions/add_layer.gd
@@ -3,9 +3,10 @@ class_name AddLayerAction
 
 var layer_data: LayerData = null
 var ground: bool = true
+var insert_index: int = -1
 
 func _do():
-	add_layer(layer_data, -1, ground)
+	add_layer(layer_data, insert_index, ground)
 
 func _undo():
 	remove_layer()
