a9b40b09728f7c03dbbfd9b065855b8bd2e93a6b
dignityloss <abdo_essam63@outlook.com>
2026-09-07 23:12:15 +0300

    torch force tint property

--- scenes/actors/objects/torch/torch.gd
+++ scenes/actors/objects/torch/torch.gd
@@ -1,7 +1,8 @@
 extends GameObject
 
+const DEFAULT_BACKGROUND_COLOR: Color = Color(0.545098, 0.545098, 0.545098)
 
-var is_background = false
+var force_tint: bool = false
 var color = Color(1, 1, 0)
 const light_color = Color(0.87, 0.65, 0.05)
 export(Array, Texture) var palette_textures
@@ -18,22 +19,35 @@ onready var sprite2 : AnimatedSprite = $AnimatedSprite/RecolorableSprite
 
 func _register_properties(): 
 	register_property(5, "color", color, true)
+	register_property(6, "force_tint", force_tint, true)
 
 
 func _ready():
 	if mode == 1:
 		# warning-ignore: unused_variable
 		connect("property_changed", self, "update_property")
+		get_tree().current_scene.get_shared_node().connect("layer_edited", self, "layer_edited")
 	
 	update_light_layer()
 	update()
+	if not force_tint: sprite.modulate = math_util.scalar_color_division(1, level_layer_ref.get_ref().modulate)
+	else: sprite.modulate = Color.white
 
 func update_property(key: String, value):
 	match key:
 		"visible", "enabled":
 			update()
+		"force_tint":
+			if not force_tint: sprite.modulate = math_util.scalar_color_division(1, level_layer_ref.get_ref().modulate)
+			else: sprite.modulate = Color.white
 
-
+func layer_edited(uuid, property, value):
+	if uuid == level_layer_ref.get_ref().layer_data.layer_metadata.layer_uuid:
+		if property == "layer_tint":
+			if not force_tint: sprite.modulate = math_util.scalar_color_division(1, value)
+			else: sprite.modulate = Color.white
+			
+	
 func _process(delta):
 	sprite2.set_frame(sprite.get_frame())
 	
