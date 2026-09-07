f486f9c22c959dd5d7cb90453c3f490913e207ce
aloelucidity <aloelucidi@gmail.com>
2026-09-07 13:03:20 +0200

    add new bush sprites and functionality

--- scenes/actors/objects/flower_bush/flower_bush.gd
+++ scenes/actors/objects/flower_bush/flower_bush.gd
@@ -1,7 +1,24 @@
-extends Decoration
+extends GameObject
 
-onready var sprite = $Sprite
+const EDGE_WIDTH: int = 32
+const PART_WIDTH: int = 64
+const HEDGE_EDGE_WIDTH: int = 16
+const HEDGE_PART_WIDTH: int = 32
+
+onready var sprite: NinePatchRect = $Sprite
+onready var recolorable: NinePatchRect = $Sprite/Recolorable
 onready var interaction_area = $Area2D
+onready var interaction_shape = $Area2D/CollisionShape2D
+
+export(Array, Texture) var palette_textures
+export(Array, Texture) var flowerless_textures
+export(Array, Texture) var recolorable_textures
+export(Array, Texture) var hedge_textures
+
+export(Array, Texture) var single_palette_textures
+export(Array, Texture) var single_flowerless_textures
+export(Array, Texture) var single_recolorable_textures
+export(Array, Texture) var single_hedge_textures
 
 var displacement : float = 0.0
 var displacement_spring_anim_power : float = 0.0
@@ -12,19 +29,63 @@ var scale_spring_anim_power : float = 0.0
 var flowers: bool = true
 var flower_color: Color = Color.yellow
 var parts: int = 1
+var hedge: bool = false
 
-
-#func _set_properties():
-#	savable_properties = ["flowers", "flower_color", "parts"]
-#	editable_properties = ["flowers", "flower_color", "parts"]
 
 func _register_properties():
 	register_property(4, "flowers", flowers, true)
 	register_property(5, "flower_color", flower_color, true)
 	register_property(6, "parts", parts, true)
+	register_property(7, "hedge", hedge, true)
 
-func _object_ready():
+
+func _ready():
+	interaction_shape.shape = interaction_shape.shape.duplicate()
 	interaction_area.connect("body_entered", self, "start_anim")
+
+	var _connect = connect("property_changed", self, "update_property")
+	update_property("palette", palette)
+	update_property("parts", parts)
+
+
+func update_property(key: String, value):
+	recolorable.modulate = flower_color
+	recolorable.visible = flowers and not hedge and (flower_color != Color.yellow)
+	
+	var palette_tex: Array = palette_textures if parts > 1 else single_palette_textures
+	var flowerless_tex: Array = flowerless_textures if parts > 1 else single_flowerless_textures
+	var recolorable_tex: Array = recolorable_textures if parts > 1 else single_recolorable_textures
+	if hedge:
+		palette_tex = hedge_textures if parts > 1 else single_hedge_textures
+		flowerless_tex = palette_tex
+	
+	sprite.texture = palette_tex[palette] if flowers else flowerless_tex[palette]
+	recolorable.texture = recolorable_tex[palette]
+	
+	var edge_width: int = HEDGE_EDGE_WIDTH if hedge else EDGE_WIDTH
+	var part_width: int = HEDGE_PART_WIDTH if hedge else PART_WIDTH
+	
+	sprite.patch_margin_left = edge_width
+	sprite.patch_margin_right = edge_width
+	
+	sprite.rect_size.x = edge_width*2 + part_width * (parts - 1)
+	sprite.rect_position.x = -sprite.rect_size.x / 2
+	sprite.rect_pivot_offset = -sprite.rect_position
+	sprite.rect_pivot_offset.y *= 2
+	editor_rect = Rect2(sprite.rect_position, sprite.rect_size)
+	interaction_shape.shape.extents.x = (sprite.rect_size.x / 2) - 12
+
+
+func _input(event):
+	if event is InputEventMouseButton and event.is_pressed() and is_object_hovered():
+		if event.button_index == 5: # Mouse wheel down
+			parts -= 1
+			if parts < 1:
+				parts = 1
+			set_property("parts", parts)
+		elif event.button_index == 4: # Mouse wheel up
+			parts += 1
+			set_property("parts", parts)
 
 
 func _object_process(delta: float) -> void:
@@ -36,7 +97,7 @@ func _object_process(delta: float) -> void:
 	if !is_equal_approx(scale_spring_anim_power, 0):
 		update_scale_spring(delta)
 	else:
-		sprite.scale = Vector2.ONE
+		sprite.rect_scale = Vector2.ONE
 
 
 func start_anim(body):
@@ -84,5 +145,5 @@ func update_scale_spring(delta):
 	scale_spring_anim_power -= force * delta
 	base_scale_factor -= scale_spring_anim_power * delta
 	
-	sprite.scale.y = (1 + base_scale_factor)
-	sprite.scale.x = 1-((sprite.scale.y-1)/2)
+	sprite.rect_scale.y = (1 + base_scale_factor)
+	sprite.rect_scale.x = 1-((sprite.rect_scale.y-1)/2)
