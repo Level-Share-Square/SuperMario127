18fb59cd643d42c68d64e0ef0e7ec302c044a24b
dignityloss <abdo_essam63@outlook.com>
2026-09-17 16:24:29 +0300

    quick music fix

--- scenes/actors/music/music.gd
+++ scenes/actors/music/music.gd
@@ -335,11 +335,14 @@ func check_loop(player: AudioStreamPlayer, loop_point_start: float, loop_point_e
 	if loop_point_start >= loop_point_end: loop_point_start = 0.0
 	var pos: float = get_precise_position(player)
 
+	prints(pos, loop_point_end, loop_point_start)
 	if pos >= loop_point_end or not player.playing:
+		if pos == 0: pos = loop_point_end
 		var overshoot: float = pos - loop_point_end
 		var seg_len: float = loop_point_end - loop_point_start
 		if seg_len > 0.0:
 			overshoot = fmod(overshoot, seg_len)
 
 		player.playing = true
+		prints(loop_point_start + overshoot, loop_point_start, overshoot)
 		player.seek(loop_point_start + overshoot)
