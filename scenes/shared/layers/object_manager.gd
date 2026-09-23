dd2579c58729b87b46b0888f2d1470db11da1858
Plug N' Play <31835840+CardboardCarl@users.noreply.github.com>
2026-09-22 23:49:03 -0400

    fix editor launch star visual bug in fluids

--- scenes/shared/layers/object_manager.gd
+++ scenes/shared/layers/object_manager.gd
@@ -2,22 +2,24 @@ class_name ObjectManager
 extends Node2D
 
 const WATER_ID := 72 #water bug my behated
-var water_stack: Array 
+const LAVA_ID := 75
+const QUICKSAND_ID := 142
+var fluid_stack: Array 
 
 var layer_data: LayerData
 
 func load_in(s_layer_data: LayerData):
 	layer_data = s_layer_data
-	water_stack.clear()
+	fluid_stack.clear()
 	for child in get_children():
 		child.queue_free()
 	
 	for object_data in layer_data.object_data:
 		var obj = create_object(object_data)
 		add_child(obj)
-		if _is_water(object_data):
-			water_stack.append(obj)
-	_push_water_to_top()
+		if _is_fluid(object_data):
+			fluid_stack.append(obj)
+	_push_fluid_to_top()
 
 
 func place_object(object_data: ObjectData, add_to_data: bool = false):
@@ -28,10 +30,10 @@ func place_object(object_data: ObjectData, add_to_data: bool = false):
 	var game_object = create_object(object_data)
 	add_child(game_object)
 	
-	if _is_water(object_data):
-		water_stack.append(game_object)
+	if _is_fluid(object_data):
+		fluid_stack.append(game_object)
 	move_child(game_object, get_child_count())
-	_push_water_to_top()
+	_push_fluid_to_top()
 	return game_object
 
 
@@ -53,8 +55,8 @@ func create_object(object_data: ObjectData):
 
 
 func erase_object(game_object, free: bool = true) -> void:
-	if game_object in water_stack:
-		water_stack.erase(game_object)
+	if game_object in fluid_stack:
+		fluid_stack.erase(game_object)
 	var object_data: ObjectData = game_object.object_data
 	game_object._object_removed(free)
 	if free: game_object.queue_free()
@@ -62,7 +64,7 @@ func erase_object(game_object, free: bool = true) -> void:
 	
 
 func reorder_object(game_object, index: int) -> void:
-	var max_index = get_child_count() - 1 - water_stack.size()
+	var max_index = get_child_count() - 1 - fluid_stack.size()
 	index = clamp(index, 0, max_index)
 	move_child(game_object, index)
 	
@@ -70,7 +72,7 @@ func reorder_object(game_object, index: int) -> void:
 	
 	layer_data.object_data.erase(object_data)
 	layer_data.object_data.insert(index, object_data)
-	_push_water_to_top()
+	_push_fluid_to_top()
 
 func get_absolute_z_index(target: Node2D) -> int:
 	var node = target;
@@ -83,12 +85,12 @@ func get_absolute_z_index(target: Node2D) -> int:
 	return z_index;
 
 
-func _is_water(object_data: ObjectData) -> bool:
-	return object_data.metadata.type_id == WATER_ID
+func _is_fluid(object_data: ObjectData) -> bool:
+	return object_data.metadata.type_id in [WATER_ID, LAVA_ID, QUICKSAND_ID]
 
 
-func _push_water_to_top() -> void:
-	for node in water_stack:
+func _push_fluid_to_top() -> void:
+	for node in fluid_stack:
 		node.raise()
 
 func refresh_object_z_index(object):
