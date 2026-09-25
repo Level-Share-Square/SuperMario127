135baed68ef97a02fc1a2d05c6c83a53eb14920a
aloelucidity <aloelucidi@gmail.com>
2026-09-25 17:54:09 +0200

    fix speech bubbles showing behind stuff

--- scenes/actors/objects/star_coin/star_coin.gd
+++ scenes/actors/objects/star_coin/star_coin.gd
@@ -99,6 +99,11 @@ func update_color():
 
 func collect(body : PhysicsBody2D) -> void:
 	if is_enabled_and_on_ground() and !collected and (body is Character):
+		if not Singleton.ModeSwitcher.visible and CurrentLevelData.save_data.get_collected_star_coin_count() + 1 == CurrentLevelData.level_metadata.collectible_data.get_star_coin_count():
+			audio_player_complete.play()
+		else:
+			audio_player.play()
+		
 		if not Singleton.ModeSwitcher.visible:
 			CurrentLevelData.save_data.set_star_coin_collected(uuid, CurrentLevelData.selected_file > -2)
 
@@ -108,11 +113,6 @@ func collect(body : PhysicsBody2D) -> void:
 		LastInputDevice.rumble(0.5, 0.8, 0.2)
 		animation_player.play("collect")
 		var _connect = animation_player.connect("animation_finished", self, "queue_free")
-
-		if not Singleton.ModeSwitcher.visible and CurrentLevelData.save_data.get_collected_star_coin_count() == CurrentLevelData.level_metadata.collectible_data.get_star_coin_count():
-			audio_player_complete.play()
-		else:
-			audio_player.play()
 
 
 func _object_removed(free: bool) -> void:
