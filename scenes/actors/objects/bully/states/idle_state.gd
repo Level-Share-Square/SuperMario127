56d70fcd52ca28aabc96817f56599ba0b142dcb1
BockyBlock <97856812+BockyBlock@users.noreply.github.com>
2026-09-25 17:20:00 -0400

    Bullies no longer enter the "chase" state midair
    Bullies can no longer enter the "chase" state midair. This is because switching to that state would cause them to lose all of their horizontal momentum., and drop like a rock.
    
    If you have Bullies set to spawn onscreen with horizontal momentum, this is useful!

--- scenes/actors/objects/bully/states/idle_state.gd
+++ scenes/actors/objects/bully/states/idle_state.gd
@@ -11,11 +11,11 @@ func _update(_delta: float) -> void:
 	
 	if enemy.is_on_ground():
 		enemy.sprite.play("idle")
+	
+		if is_instance_valid(enemy.player_detector.get_player()):
+			enemy.set_state_by_name("ChaseState")
 	else:
 		if enemy.velocity.y > 0:
 			enemy.sprite.play("fall")
 		else:
 			enemy.sprite.play("jump")
-	
-	if is_instance_valid(enemy.player_detector.get_player()):
-		enemy.set_state_by_name("ChaseState")
