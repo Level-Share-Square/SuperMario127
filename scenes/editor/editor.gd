890275021490b87d7685f1b8c5b70e1c069846de
dignityloss <abdo_essam63@outlook.com>
2026-09-26 00:40:13 +0300

    fix object pen selection

--- scenes/editor/editor.gd
+++ scenes/editor/editor.gd
@@ -102,12 +102,10 @@ func get_hovered_objects():
 	var hovered_objects: Dictionary = {}
 	var check_layer = get_shared_node().get_layer(layer)
 	if not is_instance_valid(check_layer): return
-
-	var screen_rect := Rect2(Vector2.ZERO, get_viewport_rect().size)
 	
 	for index in range(check_layer.object_manager.get_child_count()):
 		var object = check_layer.object_manager.get_child(index)
-		if screen_rect.has_point(object.get_global_transform_with_canvas().origin) and object.is_object_hovered():
+		if object.is_object_hovered():
 			hovered_objects.get_or_add(object.name, object)
 	item_preview_hovered_objects = hovered_objects
 	return hovered_objects
