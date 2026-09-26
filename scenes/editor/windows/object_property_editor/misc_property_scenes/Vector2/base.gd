e167f2779390d689cb7437b7f6c95cdf36596d31
BlockCycles <blockcycle40@gmail.com>
2026-09-26 00:13:53 -0300

    Fixed recursivity in Vector2 field's 'property_changed' method

--- scenes/editor/windows/object_property_editor/misc_property_scenes/Vector2/base.gd
+++ scenes/editor/windows/object_property_editor/misc_property_scenes/Vector2/base.gd
@@ -1,10 +1,13 @@
 extends PropertyEditor
 
 func property_changed(key: String, new_value):
+	#print("Reached property_change of Vector2/base.gd")
 	if key != property[0]: return
+	#print("Matched property")
 	if not new_value is Vector2: return
-	$X.value = new_value.x
-	$Y.value = new_value.y
+	#print("Matched new_value type")
+	$X.set_value_no_signal(float(new_value.x)) # Directly changing SpinBox.value (and maybe some other fields as well)
+	$Y.set_value_no_signal(float(new_value.y)) # triggers the signal. Avoid using it to just update the field.
 
 func load_property(_editor: Editor, init_value, _property: Array, property_name = null):
 	.load_property(_editor, init_value, _property, property_name)
@@ -24,6 +27,9 @@ func load_property(_editor: Editor, init_value, _property: Array, property_name 
 		
 		$Y.prefix = property_info.prefix[1]
 		$Y.suffix = property_info.suffix[1]
+	# (yep, these 2 lines are necessary)
+	$X.set_value_no_signal(float(init_value.x))
+	$Y.set_value_no_signal(float(init_value.y))
 
 func change_property(_new_value = null, save_to_data: bool = true):
 	var new_value := Vector2(
